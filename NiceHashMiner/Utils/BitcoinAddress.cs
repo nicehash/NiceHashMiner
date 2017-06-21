45908ef9bbebfa1729476226701c612ac81c91ae
Dillon Newell <dillon_newell@live.ca>
2017-06-21 12:39:28 -0400

    Don't allow spaces in workername

--- NiceHashMiner/Utils/BitcoinAddress.cs
+++ NiceHashMiner/Utils/BitcoinAddress.cs
@@ -28,7 +28,7 @@ namespace NiceHashMiner
 
         public static bool ValidateWorkerName(string workername)
         {
-            if (workername.Length > 7 || !isAlphaNumeric(workername))
+            if (workername.Length > 7 || !isAlphaNumeric(workername) || workername.Contains(" "))
                 return false;
 
             return true;
