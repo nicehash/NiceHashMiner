3115b5135c63aeaa41b048d5358ccb75142f4129
Dillon Newell <dillon_newell@live.ca>
2017-07-06 18:29:18 -0400

    check update for NHML

--- NiceHashMiner/Utils/Links.cs
+++ NiceHashMiner/Utils/Links.cs
@@ -4,13 +4,13 @@ using System.Text;
 
 namespace NiceHashMiner {
     public static class Links {
-        public static string VisitURL = "https://www.nicehash.com?utm_source=NHM";
+        public static string VisitURL = "https://github.com/DillonN/NiceHashMiner";
         // add version
-        public static string VisitURLNew = "https://github.com/nicehash/NiceHashMiner/releases/tag/";
+        public static string VisitURLNew = "https://github.com/NiceHash/NiceHashMinerLegacy/releases/tag/";
         // add btc adress as parameter
         public static string CheckStats = "https://www.nicehash.com/index.jsp?utm_source=NHM&p=miners&addr=";
         // help and faq
-        public static string NHM_Help = "https://github.com/nicehash/NiceHashMiner";
+        public static string NHM_Help = "https://github.com/DillonN/NiceHashMiner/issues";
         // faq
         public static string NHM_BTC_Wallet_Faq = "https://www.nicehash.com/index.jsp?utm_source=NHM&p=faq#faqs15";
         public static string NHM_Paying_Faq = "https://www.nicehash.com/index.jsp?utm_source=NHM&p=faq#faqs6";
@@ -18,7 +18,7 @@ namespace NiceHashMiner {
         // btc adress as parameter
         public static string NHM_API_stats = "https://api.nicehash.com/api?method=stats.provider&addr=";
         public static string NHM_API_info = "https://api.nicehash.com/api?method=simplemultialgo.info";
-        public static string NHM_API_version = "https://api.nicehash.com/nicehashminer?method=version";
+        public static string NHM_API_version = "https://api.nicehash.com/nicehashminer?method=version&legacy";
         //public static string NHM_API_stats_provider_workers = "https://api.nicehash.com/api?method=stats.provider.workers&addr=";
 
         // device profits
