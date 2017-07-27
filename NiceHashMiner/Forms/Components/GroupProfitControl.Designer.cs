654d0b1ae4403255802bdc56dfca1f02168aa442
DillonN <dillon_newell@live.ca>
2017-07-27 01:07:24 -0400

    Scaling for groupprofitcontrol

--- NiceHashMiner/Forms/Components/GroupProfitControl.Designer.cs
+++ NiceHashMiner/Forms/Components/GroupProfitControl.Designer.cs
@@ -41,10 +41,8 @@
             this.groupBoxMinerGroup.Controls.Add(this.labelSpeedValue);
             this.groupBoxMinerGroup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.groupBoxMinerGroup.Location = new System.Drawing.Point(0, 0);
-            this.groupBoxMinerGroup.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBoxMinerGroup.Name = "groupBoxMinerGroup";
-            this.groupBoxMinerGroup.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBoxMinerGroup.Size = new System.Drawing.Size(804, 49);
+            this.groupBoxMinerGroup.Size = new System.Drawing.Size(536, 32);
             this.groupBoxMinerGroup.TabIndex = 108;
             this.groupBoxMinerGroup.TabStop = false;
             this.groupBoxMinerGroup.Text = "Mining Devices { N/A } ";
@@ -53,20 +51,18 @@
             // 
             this.labelSpeedIndicator.AutoSize = true;
             this.labelSpeedIndicator.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.labelSpeedIndicator.Location = new System.Drawing.Point(9, 25);
-            this.labelSpeedIndicator.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSpeedIndicator.Location = new System.Drawing.Point(6, 16);
             this.labelSpeedIndicator.Name = "labelSpeedIndicator";
-            this.labelSpeedIndicator.Size = new System.Drawing.Size(67, 20);
+            this.labelSpeedIndicator.Size = new System.Drawing.Size(47, 13);
             this.labelSpeedIndicator.TabIndex = 108;
             this.labelSpeedIndicator.Text = "Speed:";
             // 
             // labelCurentcyPerDayVaue
             // 
             this.labelCurentcyPerDayVaue.AutoSize = true;
-            this.labelCurentcyPerDayVaue.Location = new System.Drawing.Point(651, 25);
-            this.labelCurentcyPerDayVaue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelCurentcyPerDayVaue.Location = new System.Drawing.Point(434, 16);
             this.labelCurentcyPerDayVaue.Name = "labelCurentcyPerDayVaue";
-            this.labelCurentcyPerDayVaue.Size = new System.Drawing.Size(85, 20);
+            this.labelCurentcyPerDayVaue.Size = new System.Drawing.Size(61, 13);
             this.labelCurentcyPerDayVaue.TabIndex = 104;
             this.labelCurentcyPerDayVaue.Text = "0.00 $/Day";
             // 
@@ -74,20 +70,18 @@
             // 
             this.labelBTCRateValue.AutoSize = true;
             this.labelBTCRateValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.labelBTCRateValue.Location = new System.Drawing.Point(475, 25);
-            this.labelBTCRateValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelBTCRateValue.Location = new System.Drawing.Point(317, 16);
             this.labelBTCRateValue.Name = "labelBTCRateValue";
-            this.labelBTCRateValue.Size = new System.Drawing.Size(168, 20);
+            this.labelBTCRateValue.Size = new System.Drawing.Size(112, 13);
             this.labelBTCRateValue.TabIndex = 105;
             this.labelBTCRateValue.Text = "0.00000000 BTC/Day";
             // 
             // labelBTCRateIndicator
             // 
             this.labelBTCRateIndicator.AutoSize = true;
-            this.labelBTCRateIndicator.Location = new System.Drawing.Point(419, 25);
-            this.labelBTCRateIndicator.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelBTCRateIndicator.Location = new System.Drawing.Point(279, 16);
             this.labelBTCRateIndicator.Name = "labelBTCRateIndicator";
-            this.labelBTCRateIndicator.Size = new System.Drawing.Size(48, 20);
+            this.labelBTCRateIndicator.Size = new System.Drawing.Size(33, 13);
             this.labelBTCRateIndicator.TabIndex = 106;
             this.labelBTCRateIndicator.Text = "Rate:";
             // 
@@ -95,23 +89,21 @@
             // 
             this.labelSpeedValue.AutoSize = true;
             this.labelSpeedValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.labelSpeedValue.Location = new System.Drawing.Point(84, 25);
-            this.labelSpeedValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSpeedValue.Location = new System.Drawing.Point(56, 16);
             this.labelSpeedValue.Name = "labelSpeedValue";
-            this.labelSpeedValue.Size = new System.Drawing.Size(40, 20);
+            this.labelSpeedValue.Size = new System.Drawing.Size(30, 13);
             this.labelSpeedValue.TabIndex = 107;
             this.labelSpeedValue.Text = "N/A";
             // 
             // GroupProfitControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.Controls.Add(this.groupBoxMinerGroup);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "GroupProfitControl";
-            this.Size = new System.Drawing.Size(806, 55);
+            this.Size = new System.Drawing.Size(537, 36);
             this.groupBoxMinerGroup.ResumeLayout(false);
             this.groupBoxMinerGroup.PerformLayout();
             this.ResumeLayout(false);
