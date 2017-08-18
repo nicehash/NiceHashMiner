728da6f4811a0b91a01497be7c3fb1f1bab8759b
Dillon Newell <dillon_newell@live.ca>
2017-08-18 12:45:31 -0400

    bump pre, new bin

--- NiceHashMiner/Forms/Form_Main.cs
+++ NiceHashMiner/Forms/Form_Main.cs
@@ -47,7 +47,7 @@ namespace NiceHashMiner
         int flowLayoutPanelVisibleCount = 0;
         int flowLayoutPanelRatesIndex = 0;
 
-        const string _betaAlphaPostfixString = "-Pre3";
+        const string _betaAlphaPostfixString = "-Pre4";
 
         private bool _isDeviceDetectionInitialized = false;
 
