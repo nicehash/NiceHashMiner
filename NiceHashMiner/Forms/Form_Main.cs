af3ef89cc788092717f0950e8787524890357ad0
Sav87 <Sav87612@gmail.com>
2017-11-30 04:37:55 +0800

    receiving SMA via the API

--- NiceHashMiner/Forms/Form_Main.cs
+++ NiceHashMiner/Forms/Form_Main.cs
@@ -303,7 +303,7 @@ namespace NiceHashMiner
                     --Globals.FirstNetworkCheckTimeoutTries;
                 }
             }
-
+                  
             LoadingScreen.IncreaseLoadCounterAndMessage(International.GetText("Form_Main_loadtext_GetBTCRate"));
 
             BitcoinExchangeCheck = new Timer();
@@ -603,11 +603,11 @@ namespace NiceHashMiner
             Helpers.ConsolePrint("NICEHASH", "Current Bitcoin rate: " + Globals.BitcoinUSDRate.ToString("F2", CultureInfo.InvariantCulture));
         }
 
-        void SMACallback(object sender, EventArgs e) {
+        void SMACallback(object sender, AlgorithmRatesEventArgs e) {
             Helpers.ConsolePrint("NICEHASH", "SMA Update");
             isSMAUpdated = true;
-            if (NiceHashStats.AlgorithmRates != null) {
-                Globals.NiceHashData = NiceHashStats.AlgorithmRates;
+            if (e.AlgorithmRates != null) {
+                Globals.NiceHashData = e.AlgorithmRates;
             }
         }
 
