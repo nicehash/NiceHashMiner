47032f8c12b732469946671f25258458a61bf48d
Dillon Newell <dillon@newell-labs.ca>
2018-01-09 02:52:35 -0500

    ver bump

--- NiceHashMiner/Globals.cs
+++ NiceHashMiner/Globals.cs
@@ -7,7 +7,7 @@ namespace NiceHashMiner {
     public class Globals {
         // Constants
         public static string[] MiningLocation = { "eu", "usa", "hk", "jp", "in", "br" };
-        public static readonly string DemoUser = "3DJhaQaKA6oyRaGyDZYdkZcise4b9DrCi2";
+        public static readonly string DemoUser = "33hGFJZQAfbdzyHGqhJPvZwncDjUBdZqjW";
         // change this if TOS changes
         public static int CURRENT_TOS_VER = 3;
 
