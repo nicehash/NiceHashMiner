2cb4c80eae524f17b79f1e3274df4cbdfbca5c49
Dillon Newell <dillon_newell@live.ca>
2018-01-09 04:13:22 -0500

    Add logging to existance checker

--- NiceHashMiner/Utils/MinersExistanceChecker.cs
+++ NiceHashMiner/Utils/MinersExistanceChecker.cs
@@ -8,6 +8,7 @@ namespace NiceHashMiner.Utils {
         public static bool IsMinersBins_ALL_Init() {
             foreach (var filePath in Bins_Data.ALL_FILES_BINS) {
                 if (!File.Exists(String.Format("bin{0}", filePath))) {
+                    Helpers.ConsolePrint("MinersExistanceChecker", $"bin{filePath} doesn't exist! Warning");
                     return false;
                 }
             }
@@ -17,6 +18,7 @@ namespace NiceHashMiner.Utils {
         public static bool IsMiners3rdPartyBinsInit() {
             foreach (var filePath in Bins_Data_3rd.ALL_FILES_BINS) {
                 if (!File.Exists(String.Format("bin_3rdparty{0}", filePath))) {
+                    Helpers.ConsolePrint("MinersExistanceChecker", $"bin_3rdparty{filePath} doesn't exist! Warning");
                     return false;
                 }
             }
