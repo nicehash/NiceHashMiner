f19c41f4762e0150bdfff1e30104d4eb4afdff12
Dillon Newell <dillon@newell-labs.ca>
2018-04-09 06:49:44 -0400

    change links

--- NiceHashMiner/Utils/Links.cs
+++ NiceHashMiner/Utils/Links.cs
@@ -2,21 +2,21 @@
 {
     public static class Links
     {
-        public static string VisitUrl = "https://www.nicehash.com?utm_source=NHM";
+        public static string VisitUrl = "https://www.nicehash.com";
 
         // add version
         public static string VisitUrlNew = "https://github.com/NiceHash/NiceHashMinerLegacy/releases/tag/";
 
         // add btc adress as parameter
-        public static string CheckStats = "https://www.nicehash.com/index.jsp?utm_source=NHM&p=miners&addr=";
+        public static string CheckStats = "https://www.nicehash.com/index.jsp?p=miners&addr=";
 
         // help and faq
         public static string NhmHelp = "https://github.com/nicehash/NiceHashMinerLegacy/";
         public static string NhmNoDevHelp = "https://github.com/nicehash/NiceHashMinerLegacy/wiki/Troubleshooting#nosupportdev";
 
         // faq
-        public static string NhmBtcWalletFaq = "https://www.nicehash.com/help/how-to-create-the-bitcoin-addresswallet?utm_source=NHM";
-        public static string NhmPayingFaq = "https://www.nicehash.com/help/when-and-how-do-you-get-paid?utm_source=NHM";
+        public static string NhmBtcWalletFaq = "https://www.nicehash.com/help/how-to-create-the-bitcoin-addresswallet";
+        public static string NhmPayingFaq = "https://www.nicehash.com/help/when-and-how-do-you-get-paid";
 
         // API
         // btc adress as parameter
@@ -26,7 +26,7 @@
         //public static string NHM_API_stats_provider_workers = "https://api.nicehash.com/api?method=stats.provider.workers&addr=";
 
         // device profits
-        public static string NhmProfitCheck = "https://api.nicehash.com/?utm_source=NHM&p=calc&name=";
+        public static string NhmProfitCheck = "https://api.nicehash.com/p=calc&name=";
 
         // SMA Socket
         public static string NhmSocketAddress = "https://nhmws.nicehash.com/v2/nhm";
