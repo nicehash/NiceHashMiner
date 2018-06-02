d3d8afcffff8f46c414f78f0f76a6e8fb66d8c55
Dillon Newell <dillon@newell-labs.ca>
2018-06-02 04:16:34 -0400

    add to unix timestamp ext

---
+++ NiceHashMinerLegacy.Extensions.Tests/DateTimeTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NiceHashMinerLegacy.Extensions.Tests
+{
+    [TestClass]
+    public class DateTimeTest
+    {
+        [TestMethod]
+        public void GetUnixTime_ShouldMatch()
+        {
+            const ulong mil = 1520438617332;
+            var date = new DateTime(2018, 3, 7, 16, 3, 37, 332);
+            date = DateTime.SpecifyKind(date, DateTimeKind.Utc);
+
+            Assert.AreEqual(mil, date.GetUnixTime());
+        }
+    }
+}
---
+++ NiceHashMinerLegacy.Extensions/DateTimeExt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NiceHashMinerLegacy.Extensions
+{
+    public static class DateTimeExt
+    {
+        private static readonly DateTime UnixStart = new DateTime(1970, 1, 1);
+
+        public static ulong GetUnixTime(this DateTime time)
+        {
+            return (ulong) (time.ToUniversalTime() - UnixStart).TotalMilliseconds;
+        }
+    }
+}
