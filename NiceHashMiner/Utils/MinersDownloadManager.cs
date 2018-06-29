08b7c359972b74b832efa41e66e15287ae489df4
Dillon Newell <dillon@newell-labs.ca>
2018-06-29 11:11:42 -0400

    ver bump; new dl locs

--- NiceHashMiner/Utils/MinersDownloadManager.cs
+++ NiceHashMiner/Utils/MinersDownloadManager.cs
@@ -3,12 +3,12 @@
     public static class MinersDownloadManager
     {
         public static DownloadSetup StandardDlSetup = new DownloadSetup(
-            "http://github.com/NiceHash/NiceHashMinerLegacy/releases/download/1.9.0.3/bin_1_9_0_3.zip",
+            "http://github.com/NiceHash/NiceHashMinerLegacy/releases/download/1.9.0.4/bin_1_9_0_4.zip",
             "bins.zip",
             "bin");
 
         public static DownloadSetup ThirdPartyDlSetup = new DownloadSetup(
-            "https://github.com/NiceHash/NiceHashMinerLegacy/releases/download/1.9.0.1/bin_3rdparty_1_9_0_1.zip",
+            "https://github.com/NiceHash/NiceHashMinerLegacy/releases/download/1.9.0.4/bin_3rdparty_1_9_0_4.zip",
             "bins_3rdparty.zip",
             "bin_3rdparty");
     }
