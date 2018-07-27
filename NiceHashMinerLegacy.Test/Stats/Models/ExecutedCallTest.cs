b60a3a46b75ed0574535c13181462528c19092e0
Dillon Newell <dillon_newell@live.ca>
2018-07-26 20:32:48 -0400

    update tests

--- NiceHashMinerLegacy.Test/Stats/Models/ExecutedCallTest.cs
+++ NiceHashMinerLegacy.Test/Stats/Models/ExecutedCallTest.cs
@@ -14,16 +14,16 @@ namespace NiceHashMinerLegacy.Tests.Stats.Models
         [TestMethod]
         public void ErrorFreeShouldMatch()
         {
-            const string expected = "{\"method\":\"executed\",\"params\":[0,0]}";
-            var call = new ExecutedCall(0, null).Serialize();
+            const string expected = "{\"method\":\"executed\",\"params\":[1,0]}";
+            var call = new ExecutedCall(1, 0, null).Serialize();
             Assert.AreEqual(expected, call);
         }
 
         [TestMethod]
         public void ErrorShouldMatch()
         {
-            const string expected = "{\"method\":\"executed\",\"params\":[0,1,\"This is an error\"]}";
-            var call = new ExecutedCall(1, "This is an error").Serialize();
+            const string expected = "{\"method\":\"executed\",\"params\":[20,1,\"This is an error\"]}";
+            var call = new ExecutedCall(20, 1, "This is an error").Serialize();
             Assert.AreEqual(expected, call);
         }
     }
