da7b77908cfada83189d3598bb370912cb92f06a
Dillon Newell <dillon_newell@live.ca>
2019-01-24 16:09:35 -0500

    add gminer bin check

--- src/NiceHashMinerLegacy/Utils/BINS_CODEGEN_3rd.cs
+++ src/NiceHashMinerLegacy/Utils/BINS_CODEGEN_3rd.cs
@@ -858,7 +858,8 @@ namespace NiceHashMiner.Utils
             @"/ethlargement/OhGodAnETHlargementPill-r2.exe",
             @"/ewbf_144/miner.exe",
             @"/trex/t-rex.exe",
-            @"/phoenix/PhoenixMiner.exe"
+            @"/phoenix/PhoenixMiner.exe",
+            @"/gminer/miner.exe"
         };
 
         #endregion //CODE_GEN STUFF // listFiles.py
