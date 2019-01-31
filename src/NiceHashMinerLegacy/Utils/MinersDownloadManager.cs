58fa29370e8799e75588fcbfd4e3e2ecabda4107
Dillon Newell <dillon_newell@live.ca>
2019-01-31 18:43:59 -0500

    ver bump

--- src/NiceHashMinerLegacy/Utils/MinersDownloadManager.cs
+++ src/NiceHashMinerLegacy/Utils/MinersDownloadManager.cs
@@ -8,7 +8,7 @@
             "bin");
 
         public static DownloadSetup ThirdPartyDlSetup = new DownloadSetup(
-            "https://github.com/NiceHash/NiceHashMinerLegacy/releases/download/1.9.0.10/bin_3rdparty_1_9_0_10.zip",
+            "https://github.com/NiceHash/NiceHashMinerLegacy/releases/download/1.9.0.11/bin_3rdparty_1_9_0_11.zip",
             "bins_3rdparty.zip",
             "bin_3rdparty");
     }
