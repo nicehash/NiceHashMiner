1f2b8b85c39a8a16b21aea5c75f062c46c86d960
Dillon Newell <dillon_newell@live.ca>
2019-01-31 17:56:50 -0500

    Disable Grin

--- src/NiceHashMinerLegacy/Devices/Algorithms/DefaultAlgorithms.cs
+++ src/NiceHashMinerLegacy/Devices/Algorithms/DefaultAlgorithms.cs
@@ -241,7 +241,7 @@ namespace NiceHashMiner.Devices.Algorithms
                     new Algorithm(MinerBaseType.BMiner, AlgorithmType.Equihash),
                     // TODO Only getting CUDA errors when dual mining
                     //new DualAlgorithm(MinerBaseType.BMiner, AlgorithmType.DaggerHashimoto, AlgorithmType.Blake2s)
-                    new Algorithm(MinerBaseType.BMiner, AlgorithmType.Grin)
+                    //new Algorithm(MinerBaseType.BMiner, AlgorithmType.Grin)
                 }
             }
         }.ConcatDictList(All, Gpu);
