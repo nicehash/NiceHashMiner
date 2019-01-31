b13e81242f629634f56c2009b323421f4125cac3
Dillon Newell <dillon_newell@live.ca>
2019-01-31 18:44:37 -0500

    make dl setups readonly

--- src/NiceHashMinerLegacy/Utils/MinersDownloadManager.cs
+++ src/NiceHashMinerLegacy/Utils/MinersDownloadManager.cs
@@ -2,12 +2,12 @@
 {
     public static class MinersDownloadManager
     {
-        public static DownloadSetup StandardDlSetup = new DownloadSetup(
+        public static readonly DownloadSetup StandardDlSetup = new DownloadSetup(
             "http://github.com/NiceHash/NiceHashMinerLegacy/releases/download/1.9.0.10/bin_1_9_0_10.zip",
             "bins.zip",
             "bin");
 
-        public static DownloadSetup ThirdPartyDlSetup = new DownloadSetup(
+        public static readonly DownloadSetup ThirdPartyDlSetup = new DownloadSetup(
             "https://github.com/NiceHash/NiceHashMinerLegacy/releases/download/1.9.0.11/bin_3rdparty_1_9_0_11.zip",
             "bins_3rdparty.zip",
             "bin_3rdparty");
