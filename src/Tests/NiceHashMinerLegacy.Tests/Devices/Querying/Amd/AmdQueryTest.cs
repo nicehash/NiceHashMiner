e5d5565ded72e610836269b411f99879c2771130
Dillon Newell <dillon_newell@live.ca>
2019-02-25 23:34:59 -0500

    add test dummy classes

--- src/Tests/NiceHashMinerLegacy.Tests/Devices/Querying/Amd/AmdQueryTest.cs
+++ src/Tests/NiceHashMinerLegacy.Tests/Devices/Querying/Amd/AmdQueryTest.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NiceHashMiner.Devices.Querying.Amd;
+using NiceHashMiner.Devices.Querying.Amd.OpenCL;
 
 namespace NiceHashMinerLegacy.Tests.Devices.Querying.Amd
 {
     [TestClass]
     public class AmdQueryTest
     {
+        private class AmdQueryDummy : AmdQuery
+        {
+            private class OpenCLQueryDummy : QueryOpenCL
+            {
+                protected override string GetQueryString()
+                {
+                    return OclTestData.TestData1;
+                }
+            }
+
+            public AmdQueryDummy() : base(0)
+            {
+                OclQuery = new OpenCLQueryDummy();
+            }
+        }
+
+
     }
 }
