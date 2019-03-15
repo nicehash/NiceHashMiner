d06078bea65d8d64c0ce1ceb3146e43d92901e82
Stanko <krsticch@gmail.com>
2019-03-15 13:05:22 +0100

    work in progress

--- src/NiceHashMinerLegacy/Plugin/MinerPluginsManager.cs
+++ src/NiceHashMinerLegacy/Plugin/MinerPluginsManager.cs
@@ -11,14 +11,13 @@ using MinerPluginLoader;
 using Newtonsoft.Json;
 using NiceHashMiner.Algorithms;
 using NiceHashMiner.Devices;
+using NiceHashMinerLegacy.Common;
 
 // TODO fix up the namespace
 namespace NiceHashMiner.Plugin
 {
     public static class MinerPluginsManager
     {
-        const string PluginsPath = "miner_plugins";
-
         private static List<PluginPackageInfo> OnlinePlugins { get; set; }
         private static Dictionary<string, IMinerPlugin> MinerPlugins { get => MinerPluginHost.MinerPlugin; }
 
@@ -26,7 +25,7 @@ namespace NiceHashMiner.Plugin
 
         public static void LoadMinerPlugins()
         {
-            MinerPluginHost.LoadPlugins(PluginsPath, SearchOption.AllDirectories);
+            MinerPluginHost.LoadPlugins(Paths.MinerPluginsPath(), SearchOption.AllDirectories);
             UpdatePluginAlgorithms();
             // cross reference local and online list
             CrossReferenceInstalledWithOnline();
@@ -67,7 +66,7 @@ namespace NiceHashMiner.Plugin
         {
             try
             {
-                var deletePath = Path.Combine(PluginsPath, pluginUUID);
+                var deletePath = Path.Combine(Paths.MinerPluginsPath(), pluginUUID);
                 MinerPluginHost.MinerPlugin.Remove(pluginUUID);
                 RemovePluginAlgorithms(pluginUUID);
                 CrossReferenceInstalledWithOnline();
