583be1226bc3a1f8a394cbbb1a1e19f42240c60d
Stanko <krsticch@gmail.com>
2019-03-28 17:46:28 +0100

    ver bump, update download links, xmr-stak gen-settings quickfix

--- src/NiceHashMinerLegacy/Utils/MinersDownloadManager.cs
+++ src/NiceHashMinerLegacy/Utils/MinersDownloadManager.cs
@@ -3,12 +3,12 @@
     public static class MinersDownloadManager
     {
         public static readonly DownloadSetup StandardDlSetup = new DownloadSetup(
-            "https://github.com/nicehash/NiceHashMinerLegacy/releases/download/1.9.0.18/bin_1_9_0_19.zip",
+            "https://github.com/nicehash/NiceHashMinerLegacy/releases/download/1.9.0.19/bin_1_9_0_20.zip",
             "bins.zip",
             "bin");
 
         public static readonly DownloadSetup ThirdPartyDlSetup = new DownloadSetup(
-            "https://github.com/nicehash/NiceHashMinerLegacy/releases/download/1.9.0.18/bin_3rdparty_1_9_0_19.zip",
+            "https://github.com/nicehash/NiceHashMinerLegacy/releases/download/1.9.0.19/bin_3rdparty_1_9_0_20.zip",
             "bins_3rdparty.zip",
             "bin_3rdparty");
     }
