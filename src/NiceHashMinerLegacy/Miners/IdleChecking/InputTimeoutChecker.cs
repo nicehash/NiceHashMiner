8d1ef876374f5eebdae585fa3ae184a7a09dd016
Stanko <krsticch@gmail.com>
2019-03-29 13:57:23 +0100

    Add Miners/IdleChecking

---
+++ src/NiceHashMinerLegacy/Miners/IdleChecking/InputTimeoutChecker.cs
@@ -0,0 +1,46 @@
+﻿using NiceHashMiner.Configs;
+using System.Timers;
+
+namespace NiceHashMiner.Miners.IdleChecking
+{
+    internal class InputTimeoutChecker : IdleChecker
+    {
+        private readonly Timer _timer = new Timer(500);
+        private bool _isIdling;
+
+        public override void StartChecking()
+        {
+            _timer.Elapsed += TimerOnElapsed;
+            _timer.Start();
+        }
+
+        private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            var msIdle = Helpers.GetIdleTime();
+
+            if (msIdle < ConfigManager.GeneralConfig.MinIdleSeconds * 1000)
+            {
+                if (_isIdling)
+                {
+                    FireStatusEvent(false);
+                    _isIdling = false;
+                }
+            }
+            else
+            {
+                if (!_isIdling)
+                {
+                    FireStatusEvent(true);
+                    _isIdling = true;
+                }
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+
+            _timer.Dispose();
+        }
+    }
+}
