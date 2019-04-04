0e966b36dc6a040feae0e87d4582855b49fdbd0e
luc1an24 <domen.kirnkrefl@nicehash.com>
2019-04-04 15:41:57 +0200

    claymore plugin

---
+++ src/Miners/MinerPluginToolkitV1/Checkers.cs
@@ -0,0 +1,22 @@
+﻿using NiceHashMinerLegacy.Common.Device;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerPluginToolkitV1
+{
+    public static class Checkers
+    {
+        public static bool IsGcn4(AMDDevice dev)
+        {
+            if (dev.Name.Contains("Vega"))
+                return true;
+            if (dev.InfSection.ToLower().Contains("polaris"))
+                return true;
+
+            return false;
+        }
+    }
+}
