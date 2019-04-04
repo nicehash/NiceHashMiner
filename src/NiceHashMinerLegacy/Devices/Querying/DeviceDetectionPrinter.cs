00ad0784ae6a67e7dc805cd1128ea68688961f59
S74nk0 <krsticch@gmail.com>
2019-04-04 23:28:12 +0200

    simplify DeviceDetectionPrinter,  fixes deadlock

--- src/NiceHashMinerLegacy/Devices/Querying/DeviceDetectionPrinter.cs
+++ src/NiceHashMinerLegacy/Devices/Querying/DeviceDetectionPrinter.cs
@@ -20,83 +20,41 @@ namespace NiceHashMiner.Devices.Querying
                     FileName = "DeviceDetectionPrinter.exe",
                     Arguments = args,
                     UseShellExecute = false,
-                    RedirectStandardError = true,
                     RedirectStandardOutput = true,
                     CreateNoWindow = true,
                 },
                 EnableRaisingEvents = true,
             };
-
-            var resultLock = new object();
-            bool resultFound = false;
-            string lineReadData = "";
-
+            string readData = "";
             T result = null;
-            var tcs = new TaskCompletionSource<T>();
-            
-            var lineRead = new DataReceivedEventHandler((object sender, DataReceivedEventArgs e) =>
-            {
-                lock (resultLock)
-                {
-                    if (resultFound) return;
-                    if (string.IsNullOrEmpty(e.Data)) return;
-                    if (string.IsNullOrWhiteSpace(e.Data)) return;
-
-                    try
-                    {
-                        lineReadData += e.Data;
-                        var isResult = JsonConvert.DeserializeObject<T>(lineReadData, Globals.JsonSettings);
-                        if (isResult != null)
-                        {
-                            resultFound = true;
-                            tcs.TrySetResult(isResult);
-                            run.Close();
-                        }
-                    }
-                    catch
-                    { }
-                }
-            });
             try
             {
-                run.OutputDataReceived += lineRead;
                 if (!run.Start())
                 {
                     throw new InvalidOperationException("Could not start process: " + run);
                 }
                 var ct = new CancellationTokenSource(milliseconds);
-                ct.Token.Register(() => {
-                    try {
-                        tcs.TrySetCanceled();
-                    }
-                    catch{}
-                });
-                run.BeginOutputReadLine();
-                // TODO this here is hacky
-                var waitForExit = Task.Run(() =>
+                ct.Token.Register(() =>
                 {
-                    if (milliseconds < 0)
+                    try
                     {
-                        run.WaitForExit();
+                        run.Kill();
                     }
-                    else
-                    {
-                        run.WaitForExit(milliseconds);
-                    }
+                    catch { }
                 });
-                result = await tcs.Task;
-                if (result == null)
+                readData = await run.StandardOutput.ReadToEndAsync();
+                result = JsonConvert.DeserializeObject<T>(readData, Globals.JsonSettings);
+                if (result == null && !string.IsNullOrEmpty(readData))
                 {
-                    Helpers.ConsolePrint("DeviceDetectionPrinter", $"result is NULL lineReadData='{lineReadData}'");
+                    Helpers.ConsolePrint("DeviceDetectionPrinter", $"result is NULL readData='{readData}'");
                 }
             }
             catch (Exception ex)
             {
-                Helpers.ConsolePrint("DeviceDetectionPrinter", $"threw Exception: {ex.Message}");
+                Helpers.ConsolePrint("DeviceDetectionPrinter", $"threw Exception: '{ex.Message}'. ReadData '{readData}'");
             }
             finally
             {
-                run.OutputDataReceived -= lineRead;
                 run.Dispose();
             }
 
