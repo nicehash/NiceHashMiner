4bcdbc8c582e179dfea7663cb736ad3e04cd2fdb
luc1an24 <domen.kirnkrefl@nicehash.com>
2019-04-23 09:27:09 +0200

    basic scenario for parser

--- Miners/MinerPluginToolkitV1/ExtraLaunchParameters/ParserTest.cs
+++ Miners/MinerPluginToolkitV1/ExtraLaunchParameters/ParserTest.cs
@@ -6,7 +6,7 @@ using NiceHashMinerLegacy.Common.Device;
 using NiceHashMinerLegacy.Common.Enums;
 using System.Collections.Generic;
 
-namespace Miners.MinerPluginToolkitV1.ExtraLaunchParameters
+namespace Miners.MinerPluginToolkitV1.ExtraLaunchParameters.Test
 {
     [TestClass]
     public class ParserTest
@@ -15,15 +15,36 @@ namespace Miners.MinerPluginToolkitV1.ExtraLaunchParameters
         public void Parse_Test()
         {
             var device1 = new BaseDevice(DeviceType.NVIDIA, "GPU-d97bdb7c-4155-9124-31b7-4743e16d3ac0", "MSI GeForce GTX 1070 Ti", 0);
-            var algorithm1 = new Algorithm();
-            var pair = new MiningPair
+            var algorithm1 = new Algorithm("BMiner", AlgorithmType.GrinCuckaroo29);
+            algorithm1.ExtraLaunchParameters = "-intensity 10";
+            var pair1 = new MiningPair
             {
                 Device = device1,
-                Algorithm = 
+                Algorithm = algorithm1
             };
-            var miningPairs = new List<MiningPair>();
+            var miningPairs = new List<MiningPair>() { pair1};
+            var options = new List<MinerOption>
+            {
+                new MinerOption
+                {
+                    Type = MinerOptionType.OptionWithSingleParameter,
+                    ID = "bminer_dual_subsolver",
+                    ShortName = "-dual-subsolver",
+                    DefaultValue = "-1"
+                },
+                new MinerOption
+                {
+                    Type = MinerOptionType.OptionWithSingleParameter,
+                    ID = "bminer_cpu_intensity",
+                    ShortName = "-intensity",
+                    DefaultValue = "6"
+                }
+            };
 
-            var options = new List<MinerOption>();
+            var ret = Parser.Parse(miningPairs, options);
+            var exp = "-intensity 10";
+
+            Assert.AreEqual(ret, exp);
         }
     }
 }
