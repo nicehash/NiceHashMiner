73dc31b9090daba58258f4ff521a01c77f15e306
Stanko <krsticch@gmail.com>
2019-04-26 13:40:14 +0200

    IDE warnings disable

--- src/NiceHashMinerLegacy/Miners/IntegratedPlugins/CCMinerIntegrated/CCMinerTPruvotIntegratedPlugin.cs
+++ src/NiceHashMinerLegacy/Miners/IntegratedPlugins/CCMinerIntegrated/CCMinerTPruvotIntegratedPlugin.cs
@@ -33,6 +33,7 @@ namespace NiceHashMiner.Miners.IntegratedPlugins
 
             foreach (var gpu in cudaGpus)
             {
+#pragma warning disable 0618
                 var algorithms = new List<Algorithm> {
                     new Algorithm(PluginUUID, AlgorithmType.NeoScrypt),
                     new Algorithm(PluginUUID, AlgorithmType.Blake2s),
@@ -41,6 +42,7 @@ namespace NiceHashMiner.Miners.IntegratedPlugins
                     new Algorithm(PluginUUID, AlgorithmType.Lyra2REv3),
                     new Algorithm(PluginUUID, AlgorithmType.X16R), // TODO check performance
                 };
+#pragma warning restore 0618
                 supported.Add(gpu, algorithms);
             }
 
