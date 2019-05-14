b8c276285118bf0e942d39fcf5eaa1b9da671315
Stanko <krsticch@gmail.com>
2019-05-14 12:58:23 +0200

    version update change miner bins download links

--- src/NiceHashMinerLegacy/Utils/MinersDownloadManager.cs
+++ src/NiceHashMinerLegacy/Utils/MinersDownloadManager.cs
@@ -3,12 +3,12 @@
     public static class MinersDownloadManager
     {
         public static readonly DownloadSetup StandardDlSetup = new DownloadSetup(
-            "https://github.com/nicehash/NiceHashMinerLegacy/releases/download/1.9.0.19/bin_1_9_0_20.zip",
+            "https://github.com/nicehash/NiceHashMinerLegacy/releases/download/1.9.0.20/bin_1_9_0_21.zip",
             "bins.zip",
             "bin");
 
         public static readonly DownloadSetup ThirdPartyDlSetup = new DownloadSetup(
-            "https://github.com/nicehash/NiceHashMinerLegacy/releases/download/1.9.0.19/bin_3rdparty_1_9_0_20.zip",
+            "https://github.com/nicehash/NiceHashMinerLegacy/releases/download/1.9.0.20/bin_3rdparty_1_9_0_21.zip",
             "bins_3rdparty.zip",
             "bin_3rdparty");
     }
