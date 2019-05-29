bc31104c330a56e2071caf66b9a1f9eb6c0a83c5
Dillon Newell <dillon@newell-labs.ca>
2019-05-28 22:30:59 -0400

    add mock data; fixes

--- NiceHashMinerWpf/MainWindow.xaml.cs
+++ NiceHashMinerWpf/MainWindow.xaml.cs
@@ -23,6 +23,37 @@ namespace NiceHashMinerWpf
         public MainWindow()
         {
             InitializeComponent();
+
+            var devs = new List<DeviceInfo>
+            {
+                new DeviceInfo(false, "CPU#1 Intel(R) Core(TM) i7-8700k CPU @ 3.70GHz", null, 10, null, "3 / 3 / 0"),
+                new DeviceInfo(true, "GPU#1 EVGA GeForce GTX 1080 Ti", 64, 0, 1550, "36 / 27 / 5"),
+                new DeviceInfo(true, "GPU#2 EVGA GeForce GTX 1080 Ti", 54, 0, 1150, "36 / 27 / 3"),
+
+            };
+
+            DevGrid.ItemsSource = devs;
+        }
+    }
+
+    public class DeviceInfo
+    {
+        public bool Enabled { get; set; }
+        public string Device { get; }
+        public string Status => Enabled ? "Stopped" : "Disabled";
+        public int? Temp { get; }
+        public int Load { get; }
+        public int? RPM { get; }
+        public string AlgoDetail { get; }
+
+        public DeviceInfo(bool enabled, string dev, int? temp, int load, int? rpm, string detail)
+        {
+            Enabled = enabled;
+            Device = dev;
+            Temp = temp;
+            Load = load;
+            RPM = rpm;
+            AlgoDetail = detail;
         }
     }
 }
