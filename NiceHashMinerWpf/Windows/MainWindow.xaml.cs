656097448be44d1d05b84abdba1d24d83fbd9999
Dillon Newell <dillon@newell-labs.ca>
2019-05-29 07:49:59 -0400

    Test with INotifyPropertyChanged and ObservableCollection

--- NiceHashMinerWpf/Windows/MainWindow.xaml.cs
+++ NiceHashMinerWpf/Windows/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using NiceHashMinerWpf.Annotations;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace NiceHashMinerWpf.Windows
@@ -8,11 +11,13 @@ namespace NiceHashMinerWpf.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ObservableCollection<DeviceInfo> _devs;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            var devs = new List<DeviceInfo>
+            _devs = new ObservableCollection<DeviceInfo>
             {
                 new DeviceInfo(false, "CPU#1 Intel(R) Core(TM) i7-8700k CPU @ 3.70GHz", null, 10, null, "3 / 3 / 0"),
                 new DeviceInfo(true, "GPU#1 EVGA GeForce GTX 1080 Ti", 64, 0, 1550, "36 / 27 / 5"),
@@ -20,16 +25,35 @@ namespace NiceHashMinerWpf.Windows
 
             };
 
-            DevGrid.ItemsSource = devs;
+            DevGrid.ItemsSource = _devs;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _devs[1].Temp++;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _devs.Add(_devs[1]);
         }
     }
 
-    public class DeviceInfo
+    public class DeviceInfo : INotifyPropertyChanged
     {
         public bool Enabled { get; set; }
         public string Device { get; }
         public string Status => Enabled ? "Stopped" : "Disabled";
-        public int? Temp { get; }
+        private int? _temp;
+        public int? Temp
+        {
+            get => _temp;
+            set
+            {
+                _temp = value;
+                SetPropertyChanged();
+            }
+        }
         public int Load { get; }
         public int? RPM { get; }
         public string AlgoDetail { get; }
@@ -43,6 +67,14 @@ namespace NiceHashMinerWpf.Windows
             Load = load;
             RPM = rpm;
             AlgoDetail = detail;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void SetPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
