3311abd141fa4ed3b9236d148b4a46e46daf5b69
luc1an24 <domen.kirnkrefl@nicehash.com>
2019-06-07 09:38:38 +0200

    fixed lolminer root path

--- src/Miners/LolMinerBeam/LolMinerBeam.cs
+++ src/Miners/LolMinerBeam/LolMinerBeam.cs
@@ -146,7 +146,7 @@ namespace LolMinerBeam
         public override Tuple<string, string> GetBinAndCwdPaths()
         {
             var pluginRoot = Path.Combine(Paths.MinerPluginsPath(), _uuid);
-            var pluginRootBins = Path.Combine(pluginRoot, "bins");
+            var pluginRootBins = Path.Combine(pluginRoot, "bins", "0.8.1");
             var binPath = Path.Combine(pluginRootBins, "lolMiner.exe");
             var binCwd = pluginRootBins;
             return Tuple.Create(binPath, binCwd);
