bd371962e73f9c8931e28529aca97b889fdf8c84
luc1an24 <domen.kirnkrefl@nicehash.com>
2019-06-27 12:54:28 +0200

    updated examplePlugin

--- src/Miners/ExamplePlugin/ExampleMiner.cs
+++ src/Miners/ExamplePlugin/ExampleMiner.cs
@@ -1,50 +1,113 @@
 ﻿using MinerPlugin;
-using MinerPluginToolkitV1;
-using MinerPluginToolkitV1.ExtraLaunchParameters;
-using Newtonsoft.Json;
-using NiceHashMinerLegacy.Common;
 using NiceHashMinerLegacy.Common.Enums;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+using System.Diagnostics;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Example
 {
+    /// <summary>
+    /// ExampleMiner class inherits IMiner to implement basic actions for miner
+    /// </summary>
     public class ExampleMiner : IMiner
     {
-        public Task<ApiData> GetMinerStatsDataAsync()
+        /// <summary>
+        /// GetMinerStatsDataAsync function is used to retrieve data from miner API
+        /// Through the course of function data is being filled to be returned at the end
+        /// </summary>
+        public async Task<ApiData> GetMinerStatsDataAsync()
         {
-            throw new NotImplementedException();
+            var apiData = new ApiData();
+            var speedsPerDevice = new Dictionary<string, IReadOnlyList<AlgorithmTypeSpeedPair>>();
+            var powerPerDevice = new Dictionary<string, int>();
+
+            apiData.AlgorithmSpeedsTotal = new List<AlgorithmTypeSpeedPair>() { new AlgorithmTypeSpeedPair(AlgorithmType.DaggerHashimoto, 107.5) };
+            apiData.PowerUsageTotal = 228;
+
+            speedsPerDevice.Add("gpuUUID1", new List<AlgorithmTypeSpeedPair>() { new AlgorithmTypeSpeedPair(AlgorithmType.DaggerHashimoto, 65.1 ) });
+            powerPerDevice.Add("gpuUUID1", 120);
+
+            speedsPerDevice.Add("gpuUUID2", new List<AlgorithmTypeSpeedPair>() { new AlgorithmTypeSpeedPair(AlgorithmType.DaggerHashimoto, 42.4 ) });
+            powerPerDevice.Add("gpuUUID2", 108);
+
+            apiData.AlgorithmSpeedsPerDevice = speedsPerDevice;
+            apiData.PowerUsagePerDevice = powerPerDevice;
+
+            return apiData;
         }
 
+        /// <summary>
+        /// InitMiningLocationAndUsername function is used to set mining location, username and password
+        /// </summary>
         public void InitMiningLocationAndUsername(string miningLocation, string username, string password = "x")
         {
-            throw new NotImplementedException();
+            // Initialization of mining location, username and password is made here
         }
 
+        /// <summary>
+        /// In InitMiningPairs Device-Algorithm (miningPair) pairs are initialized
+        /// </summary>
         public void InitMiningPairs(IEnumerable<MiningPair> miningPairs)
         {
-            throw new NotImplementedException();
+           // Initialization of mining pairs is made here
         }
 
+        /// <summary>
+        /// StartBenchmark starts benchmark process and awaits its results which are then used for accurate switching
+        /// </summary>
         public Task<BenchmarkResult> StartBenchmark(CancellationToken stop, BenchmarkPerformanceType benchmarkType = BenchmarkPerformanceType.Standard)
         {
-            throw new NotImplementedException();
+            // set the process and get the benchmark data
+            // then return that data as BenchmarkResult
+            var result = new Task<BenchmarkResult>(
+                () => {
+                    return new BenchmarkResult
+                    {
+                        AlgorithmTypeSpeeds = new List<AlgorithmTypeSpeedPair> { new AlgorithmTypeSpeedPair(AlgorithmType.DaggerHashimoto, 62.5) },
+                        Success = true,
+                        ErrorMessage = ""
+                    };
+                });
+            
+            return result;
         }
 
+        string miningState = "STOP";
+
+        /// <summary>
+        /// Start mining process when StartMining is called
+        /// </summary>
         public void StartMining()
         {
-            throw new NotImplementedException();
+            if (miningState == "START") return;
+            var proc = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "miner.exe",
+                    Arguments = "--algo dagger --url nicehashAlgoUrl --user userBTC",
+                    WorkingDirectory = "WorkingDir"
+                },
+                EnableRaisingEvents = true
+            };
+            proc.Start();
+            miningState = "START";
         }
 
+        /// <summary>
+        /// End mining process when StopMining is called
+        /// </summary>
         public void StopMining()
         {
-            throw new NotImplementedException();
+            if (miningState == "STOP") return;
+            foreach (var process in Process.GetProcessesByName("miner"))
+            {
+                process.Kill();
+            }
+            miningState = "STOP";
         }
     }
 }
--- src/Miners/ExamplePlugin/ExamplePlugin.cs
+++ src/Miners/ExamplePlugin/ExamplePlugin.cs
@@ -1,48 +1,82 @@
 ﻿using MinerPlugin;
-using MinerPluginToolkitV1;
-using MinerPluginToolkitV1.Configs;
-using MinerPluginToolkitV1.ExtraLaunchParameters;
-using MinerPluginToolkitV1.Interfaces;
-using NiceHashMinerLegacy.Common;
 using NiceHashMinerLegacy.Common.Algorithm;
 using NiceHashMinerLegacy.Common.Device;
 using NiceHashMinerLegacy.Common.Enums;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Example
 {
     /// <summary>
-    /// In plugin class we set all settings used by miner (for example pluginUUID, GetSupportedAlgorithms, Internal Settings and more)
-    /// Interfaces from MinerPlugin and MinerPluginToolkitV1 provide large set of settings available for use
+    /// Plugin class inherits IMinerPlugin interface for registering plugin
     /// </summary>
     public class ExamplePlugin : IMinerPlugin
     {
-        public Version Version => throw new NotImplementedException();
+        public string Name => "ExamplePlugin";
 
-        public string Name => throw new NotImplementedException();
+        /// <summary>
+        /// Developer sets his email/name/nickname as Author so it can be used for contact information in case of any issues.
+        /// </summary>
+        public string Author => "developer@email.com";
 
-        public string Author => throw new NotImplementedException();
+        /// <summary>
+        /// PluginUUID is an unique user identifier which you can generate online (you can use https://www.uuidgenerator.net/ for example)
+        /// <summary>
+        public string PluginUUID => "455c4d98-a45d-45d6-98ca-499ce866b2c7";
 
-        public string PluginUUID => throw new NotImplementedException();
+        /// <summary>
+        /// With version we set the version of the plugin for further updating capabilities.
+        /// </summary>
+        public Version Version => new Version(1, 0);
 
+        /// <summary>
+        /// CanGroup checks if miner can run multiple devices with same algorithm in one miner instance
+        /// In following example we check if the algorithm type of first pair is DaggerHashimoto - in that case we don't group them
+        /// Otherwise we check if Algorithm of first pair is same to algorithm in second pair and group them if they are same
+        /// </summary>
         public bool CanGroup(MiningPair a, MiningPair b)
         {
-            throw new NotImplementedException();
+            if (a.Algorithm.FirstAlgorithmType == AlgorithmType.DaggerHashimoto) return false;
+            return a.Algorithm.FirstAlgorithmType == b.Algorithm.FirstAlgorithmType;
         }
 
+        /// <summary>
+        /// Creates a new miner instance
+        /// </summary>
         public IMiner CreateMiner()
         {
-            throw new NotImplementedException();
+            return new ExampleMiner();
         }
 
+        /// <summary>
+        /// GetSupportedAlgorithms returns algorithms supported by Miner.
+        /// You can also apply hardware filters here.
+        /// </summary>
         public Dictionary<BaseDevice, IReadOnlyList<Algorithm>> GetSupportedAlgorithms(IEnumerable<BaseDevice> devices)
         {
-            throw new NotImplementedException();
+            var supported = new Dictionary<BaseDevice, IReadOnlyList<Algorithm>>();
+
+            // we loop through devices and add supported algorithms for that device to dictionary
+            foreach (var device in devices)
+            {
+                if (device.DeviceType == DeviceType.CPU)
+                {
+                    supported.Add(device, new List<Algorithm> { new Algorithm(PluginUUID, AlgorithmType.CryptoNightR) });
+                }
+                else if(device.DeviceType == DeviceType.AMD)
+                {
+                    supported.Add(device, new List<Algorithm> { new Algorithm(PluginUUID, AlgorithmType.DaggerHashimoto) });
+                }
+                else // NVIDIA
+                {
+                    supported.Add(device, new List<Algorithm> {
+                        new Algorithm(PluginUUID, AlgorithmType.DaggerHashimoto),
+                        new Algorithm(PluginUUID, AlgorithmType.GrinCuckaroo29)
+                    });
+                }
+            }
+
+            return supported;
         }
     }
 }
