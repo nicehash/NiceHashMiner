bbe3f1240ff050f13fbd358b4ef5500d0dc16515
luc1an24 <domen.kirnkrefl@nicehash.com>
2019-06-27 07:59:27 +0200

    changed example plugin to bare minimum - not yet implemented functions

---
+++ src/Miners/ExamplePlugin/ExampleMiner.cs
@@ -0,0 +1,50 @@
+﻿using MinerPlugin;
+using MinerPluginToolkitV1;
+using MinerPluginToolkitV1.ExtraLaunchParameters;
+using Newtonsoft.Json;
+using NiceHashMinerLegacy.Common;
+using NiceHashMinerLegacy.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    public class ExampleMiner : IMiner
+    {
+        public Task<ApiData> GetMinerStatsDataAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitMiningLocationAndUsername(string miningLocation, string username, string password = "x")
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitMiningPairs(IEnumerable<MiningPair> miningPairs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BenchmarkResult> StartBenchmark(CancellationToken stop, BenchmarkPerformanceType benchmarkType = BenchmarkPerformanceType.Standard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartMining()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopMining()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Miners/ExamplePlugin/ExamplePlugin.cs
@@ -0,0 +1,48 @@
+﻿using MinerPlugin;
+using MinerPluginToolkitV1;
+using MinerPluginToolkitV1.Configs;
+using MinerPluginToolkitV1.ExtraLaunchParameters;
+using MinerPluginToolkitV1.Interfaces;
+using NiceHashMinerLegacy.Common;
+using NiceHashMinerLegacy.Common.Algorithm;
+using NiceHashMinerLegacy.Common.Device;
+using NiceHashMinerLegacy.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    /// <summary>
+    /// In plugin class we set all settings used by miner (for example pluginUUID, GetSupportedAlgorithms, Internal Settings and more)
+    /// Interfaces from MinerPlugin and MinerPluginToolkitV1 provide large set of settings available for use
+    /// </summary>
+    public class ExamplePlugin : IMinerPlugin
+    {
+        public Version Version => throw new NotImplementedException();
+
+        public string Name => throw new NotImplementedException();
+
+        public string Author => throw new NotImplementedException();
+
+        public string PluginUUID => throw new NotImplementedException();
+
+        public bool CanGroup(MiningPair a, MiningPair b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMiner CreateMiner()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<BaseDevice, IReadOnlyList<Algorithm>> GetSupportedAlgorithms(IEnumerable<BaseDevice> devices)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
