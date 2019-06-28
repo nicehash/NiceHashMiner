70aedf0d6a3d6fb565d4012a12e0e882ac3fc13a
Dillon Newell <dillon_newell@live.ca>
2019-06-28 03:20:25 -0400

    add base vm and settings vms

---
+++ NHM.Wpf/ViewModels/BaseVM.cs
@@ -0,0 +1,17 @@
+﻿using NHM.Wpf.Annotations;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NHM.Wpf.ViewModels
+{
+    public class BaseVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ NHM.Wpf/ViewModels/Settings/SettingsBaseVM.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace NHM.Wpf.ViewModels.Settings
+{
+    public class SettingsBaseVM : BaseVM
+    {
+        public string Name { get; }
+
+        public IReadOnlyList<SettingsBaseVM> Children { get; }
+
+        protected dynamic SettingsInstance;
+
+        protected SettingsBaseVM(object settingsObj, string name)
+        {
+            Name = name;
+            SettingsInstance = settingsObj;
+        }
+    }
+}
