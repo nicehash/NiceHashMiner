0f81ce3ec4eded08c086b1371c806fdfd1b78372
Stanko <krsticch@gmail.com>
2019-07-01 14:43:06 +0200

    urls cleanup

--- src/NiceHashMinerLegacy/Plugin/MinerPluginsManager.cs
+++ src/NiceHashMinerLegacy/Plugin/MinerPluginsManager.cs
@@ -325,13 +325,11 @@ namespace NiceHashMiner.Plugin
         // TODO this here is blocking
         public static bool GetOnlineMinerPlugins()
         {
-            //const string pluginsJsonApiUrl = "https://miner-plugins.nicehash.com/api/plugins";
-            const string pluginsJsonApiUrl = "https://miner-plugins-test-dev.nicehash.com/api/plugins";
             try
             {
                 using (var client = new NoKeepAlivesWebClient())
                 {
-                    string s = client.DownloadString(pluginsJsonApiUrl);
+                    string s = client.DownloadString(Links.PluginsJsonApiUrl);
                     //// local fake string
                     //string s = Properties.Resources.pluginJSON;
                     var onlinePlugins = JsonConvert.DeserializeObject<List<PluginPackageInfo>>(s, new JsonSerializerSettings
