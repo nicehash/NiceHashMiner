9ae3026ac13d5019208bdb618e7f647ecfeb888c
Dillon Newell <dillon_newell@live.ca>
2019-07-02 23:24:31 -0400

    WIP settings vm

---
+++ NHM.Wpf/ViewModels/Settings/SettingsVM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace NHM.Wpf.ViewModels.Settings
+{
+    public class SettingsVM : BaseVM
+    {
+        private SettingsBaseVM _selectedPageVM;
+        public SettingsBaseVM SelectedPageVM
+        {
+            get => _selectedPageVM;
+            set
+            {
+                _selectedPageVM = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public IReadOnlyList<SettingsBaseVM> PageVMs { get; }
+
+        public SettingsVM()
+        {
+            PageVMs = new List<SettingsBaseVM>
+            {
+                new GeneralSettingsVM(null)
+            };
+
+            SelectedPageVM = PageVMs[0];
+        }
+    }
+}
