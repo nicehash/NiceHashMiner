f3b662d5d4ffa25f875d16680a9d383ad6b08b29
Dillon Newell <dillon@newell-labs.ca>
2019-07-05 00:16:40 -0400

    add adv switching settings

---
+++ NHM.Wpf/ViewModels/Settings/AdvancedSwitchingVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHM.Wpf.ViewModels.Settings
+{
+    public class AdvancedSwitchingVM : SettingsBaseVM
+    {
+        public AdvancedSwitchingVM(object settingsObj) 
+            : base(settingsObj, "Switching")
+        { }
+    }
+}
