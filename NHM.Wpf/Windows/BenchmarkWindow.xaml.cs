f0f8c6192af7467a56a1dc84bfb801b62e503bef
Dillon Newell <dillon@newell-labs.ca>
2019-07-10 21:34:53 -0400

    upgrade to net472 for dev

--- NHM.Wpf/Windows/BenchmarkWindow.xaml.cs
+++ NHM.Wpf/Windows/BenchmarkWindow.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.Windows;
+using NHM.Wpf.Windows.Settings.Pages;
 
 namespace NHM.Wpf.Windows
 {
     /// <summary>
     /// Interaction logic for BenchmarkWindow.xaml
     /// </summary>
-    public partial class BenchmarkWindow : Window
+    public partial class BenchmarkWindow : Window, ISettingsPage
     {
         public BenchmarkWindow()
         {
