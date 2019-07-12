64ae0172b1abb8f2c2a6808d4358c0088150df32
Dillon Newell <dillon@newell-labs.ca>
2019-07-12 07:37:09 -0400

    add install progress

--- NHM.Wpf/ViewModels/Plugins/PluginEntryVM.cs
+++ NHM.Wpf/ViewModels/Plugins/PluginEntryVM.cs
@@ -14,6 +14,45 @@ namespace NHM.Wpf.ViewModels.Plugins
         public bool IsSupported => Plugin.SupportedDevicesAlgorithms.Keys.Contains("NVIDIA");
         public string InstallString => IsSupported ? "Install" : "Not Supported";
 
+        private bool _isInstalling;
+
+        public bool IsInstalling
+        {
+            get => _isInstalling;
+            private set
+            {
+                _isInstalling = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(InstallButtonEnabled));
+            }
+        }
+
+        public bool InstallButtonEnabled => IsSupported && !IsInstalling;
+
+        private double _installProgress;
+
+        public double InstallProgress
+        {
+            get => _installProgress;
+            private set
+            {
+                _installProgress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _installStatus;
+
+        public string InstallStatus
+        {
+            get => _installStatus;
+            private set
+            {
+                _installStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
         public PluginEntryVM(PluginPackageInfoCR plugin)
         {
             Plugin = plugin;
@@ -21,7 +60,20 @@ namespace NHM.Wpf.ViewModels.Plugins
 
         public async Task InstallPlugin()
         {
-            // TODO
+            // TODO Placeholder
+
+            if (IsInstalling) return;
+
+            IsInstalling = true;
+
+            for (var i = 0d; i <= 100; i += 1)
+            {
+                InstallProgress = i;
+                InstallStatus = $"Installing: {i}%";
+                await Task.Delay(100);
+            }
+
+            IsInstalling = false;
         }
     }
 }
---
+++ NHM.Wpf/Windows/Plugins/Controls/PluginInstallProgress.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NHM.Wpf.Windows.Plugins.Controls
+{
+    /// <summary>
+    /// Interaction logic for PluginInstallProgress.xaml
+    /// </summary>
+    public partial class PluginInstallProgress : UserControl
+    {
+        public static readonly DependencyProperty ProgressProperty = DependencyProperty.Register(
+            nameof(Progress),
+            typeof(double),
+            typeof(PluginInstallProgress));
+
+        public static readonly DependencyProperty StatusProperty = DependencyProperty.Register(
+            nameof(Status),
+            typeof(string),
+            typeof(PluginInstallProgress));
+
+        public double Progress
+        {
+            get => (double) GetValue(ProgressProperty);
+            set => SetValue(ProgressProperty, value);
+        }
+
+        public string Status
+        {
+            get => (string) GetValue(StatusProperty);
+            set => SetValue(StatusProperty, value);
+        }
+
+        public PluginInstallProgress()
+        {
+            InitializeComponent();
+        }
+    }
+}
