2987ce05c5099ce65545354960297c1c3f60febe
Stanko <krsticch@gmail.com>
2019-07-16 15:05:04 +0200

    version bump

--- src/NiceHashMiner/Properties/AssemblyInfo.cs
+++ src/NiceHashMiner/Properties/AssemblyInfo.cs
@@ -42,9 +42,9 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 #if TESTNET || TESTNETDEV || PRODUCTION_NEW // NEW PRODUCTION
-[assembly: AssemblyVersion("1.9.2.8")]
-[assembly: AssemblyFileVersion("1.9.2.8")]
+[assembly: AssemblyVersion("1.9.2.9")]
+[assembly: AssemblyFileVersion("1.9.2.9")]
 #else  // OLD PRODUCTION 
-[assembly: AssemblyVersion("1.9.1.8")]
-[assembly: AssemblyFileVersion("1.9.1.8")]
+[assembly: AssemblyVersion("1.9.1.9")]
+[assembly: AssemblyFileVersion("1.9.1.9")]
 #endif
