f02dc30f26f16ec3be7dc8c88138cf57de761dd8
Stanko <krsticch@gmail.com>
2019-07-16 14:40:16 +0200

    NBMiner plugin version bump

--- src/Miners/NBMiner/NBMinerPlugin.cs
+++ src/Miners/NBMiner/NBMinerPlugin.cs
@@ -22,7 +22,7 @@ namespace NBMiner
 
         public override string PluginUUID => "6c07f7a0-7237-11e9-b20c-f9f12eb6d835";
 
-        public override Version Version => new Version(2, 0);
+        public override Version Version => new Version(2, 1);
         public override string Name => "NBMiner";
 
         public override string Author => "Dillon Newell";
