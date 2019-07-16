a92467eca0d5bcdec54820a6c7a97a4a4b70679e
Dillon Newell <dillon@newell-labs.ca>
2019-07-16 05:43:37 -0400

    add mainvm

--- NHM.Wpf/Windows/MainWindow.xaml.cs
+++ NHM.Wpf/Windows/MainWindow.xaml.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using NHM.Wpf.Annotations;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows;
-using NHM.Wpf.Windows.Common;
+﻿using NHM.Wpf.Windows.Common;
 using NHM.Wpf.Windows.Plugins;
+using System;
+using System.Windows;
 
 namespace NHM.Wpf.Windows
 {
@@ -14,21 +10,9 @@ namespace NHM.Wpf.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly ObservableCollection<DeviceInfo> _devs;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            _devs = new ObservableCollection<DeviceInfo>
-            {
-                new DeviceInfo(false, "CPU#1 Intel(R) Core(TM) i7-8700k CPU @ 3.70GHz", null, 10, null, "3 / 3 / 0"),
-                new DeviceInfo(true, "GPU#1 EVGA GeForce GTX 1080 Ti", 64, 0, 1550, "36 / 27 / 5"),
-                new DeviceInfo(true, "GPU#2 EVGA GeForce GTX 1080 Ti", 54, 0, 1150, "36 / 27 / 3"),
-
-            };
-
-            DevGrid.ItemsSource = _devs;
 
             Translations.LanguageChanged += TranslationsOnLanguageChanged;
             TranslationsOnLanguageChanged(null, null);
@@ -59,6 +43,8 @@ namespace NHM.Wpf.Windows
             plugin.ShowDialog();
         }
 
+        #region Minimize to tray stuff
+
         private void CloseMenuItem_OnClick(object sender, RoutedEventArgs e)
         {
             Close();
@@ -76,44 +62,7 @@ namespace NHM.Wpf.Windows
             if (WindowState == WindowState.Minimized) // TODO && config min to tray
                 Hide();
         }
-    }
 
-    public class DeviceInfo : INotifyPropertyChanged
-    {
-        public bool Enabled { get; set; }
-        public string Device { get; }
-        public string Status => Enabled ? "Stopped" : "Disabled";
-        private int? _temp;
-        public int? Temp
-        {
-            get => _temp;
-            set
-            {
-                _temp = value;
-                SetPropertyChanged();
-            }
-        }
-        public int Load { get; }
-        public int? RPM { get; }
-        public string AlgoDetail { get; }
-        public string ButtonText => Enabled ? "Start" : "N/A";
-
-        public DeviceInfo(bool enabled, string dev, int? temp, int load, int? rpm, string detail)
-        {
-            Enabled = enabled;
-            Device = dev;
-            Temp = temp;
-            Load = load;
-            RPM = rpm;
-            AlgoDetail = detail;
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void SetPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        #endregion
     }
 }
