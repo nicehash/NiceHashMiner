529103f137b2e575f5d12b9d6e9199800cc65f6f
Dillon Newell <dillon@newell-labs.ca>
2019-07-16 09:36:56 -0400

    quiet enabled biniding errors

---
+++ NHM.Wpf/ViewModels/Converters/NulBoolToBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NHM.Wpf.ViewModels.Converters
+{
+    public class NulBoolToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return true;
+            if (value is bool b) return b;
+
+            throw new ArgumentException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
