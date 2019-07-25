68c4b07c8de9f5dd3bd70449ec6e3b8c818927b0
Dillon Newell <dillon@newell-labs.ca>
2019-07-25 18:41:16 -0400

    mininggeneral bindings

--- NHM.Wpf/ViewModels/Settings/MiningGeneralVM.cs
+++ NHM.Wpf/ViewModels/Settings/MiningGeneralVM.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NHM.Common.Enums;
 using NiceHashMiner.Configs.Data;
 
 namespace NHM.Wpf.ViewModels.Settings
 {
     public class MiningGeneralVM : SettingsBaseVM
     {
-        public MiningGeneralVM(GeneralConfig confObj) 
+        public IEnumerable<IdleCheckType> IdleTypes { get; }
+
+        public bool Enable3rdPartyCheck
+        {
+            get => Config.Use3rdPartyMiners == Use3rdPartyMiners.YES;
+            set
+            {
+                Config.Use3rdPartyMiners = value ? Use3rdPartyMiners.YES : Use3rdPartyMiners.NO;
+            }
+        }
+
+        public int SelectedIdleType
+        {
+            get => (int) Config.IdleCheckType;
+            set { Config.IdleCheckType = (IdleCheckType) value; }
+        }
+
+        public MiningGeneralVM(GeneralConfig confObj)
             : base(confObj, "Mining")
-        { }
+        {
+            var idleTypes = new List<IdleCheckType>();
+
+            foreach (var checkType in Enum.GetValues(typeof(IdleCheckType)))
+            {
+                idleTypes.Add((IdleCheckType) checkType);
+            }
+
+            IdleTypes = idleTypes;
+        }
     }
 }
