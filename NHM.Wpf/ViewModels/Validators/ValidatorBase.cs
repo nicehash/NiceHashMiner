061b5177c814e1aadc9c41fa97a7a4cebc0eec15
Dillon Newell <dillon@newell-labs.ca>
2019-07-25 20:44:00 -0400

    validation on worker/address

---
+++ NHM.Wpf/ViewModels/Validators/ValidatorBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace NHM.Wpf.ViewModels.Validators
+{
+    public abstract class ValidatorBase<T> : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (!(value is T t))
+            {
+                return new ValidationResult(false, $"{nameof(value)} must be a {typeof(T)}");
+            }
+
+            return Validate(t, cultureInfo);
+        }
+
+        public abstract ValidationResult Validate(T value, CultureInfo cultureInfo);
+    }
+}
