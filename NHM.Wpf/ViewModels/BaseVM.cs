6e4d18835ce2eaa114fa8afc16a2fe343a74b052
Dillon Newell <dillon@newell-labs.ca>
2019-07-26 20:14:36 -0400

    split notify prop changed base to common

--- NHM.Wpf/ViewModels/BaseVM.cs
+++ NHM.Wpf/ViewModels/BaseVM.cs
@@ -1,22 +1,12 @@
-﻿using NHM.Wpf.Annotations;
+﻿using NHM.Common;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace NHM.Wpf.ViewModels
 {
-    public class BaseVM : INotifyPropertyChanged
+    public class BaseVM : NotifyChangedBase
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
         protected static IEnumerable<T> GetEnumValues<T>() where T : struct
         {
             return Enum.GetValues(typeof(T)).Cast<T>();
