bed3683f768a1a88476aeaa64493180ac2c2d4ec
luc1an24-rig <domen.kirnkrefl@nicehash.com>
2019-07-29 15:52:31 +0200

    created dir

---
+++ src/Miners/SRBMiner/SRBMiner.cs
@@ -0,0 +1,37 @@
+﻿using MinerPlugin;
+using MinerPluginToolkitV1;
+using NHM.Common.Enums;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SRBMiner
+{
+    public class SRBMiner : MinerBase
+    {
+        public override Tuple<string, string> GetBinAndCwdPaths()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<ApiData> GetMinerStatsDataAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<BenchmarkResult> StartBenchmark(CancellationToken stop, BenchmarkPerformanceType benchmarkType = BenchmarkPerformanceType.Standard)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string MiningCreateCommandLine()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Miners/SRBMiner/SRBMinerPlugin.cs
@@ -0,0 +1,43 @@
+﻿using MinerPluginToolkitV1;
+using NHM.Common.Algorithm;
+using NHM.Common.Device;
+using NHM.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRBMiner
+{
+    public class SRBMinerPlugin : PluginBase
+    {
+        public override Version Version => new Version(1,0);
+
+        public override string Name => "SRBMiner";
+
+        public override string Author => "domen.kirnkrefl@nicehash.com";
+
+        public override string PluginUUID => throw new NotImplementedException();
+
+        public override IEnumerable<string> CheckBinaryPackageMissingFiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Dictionary<BaseDevice, IReadOnlyList<Algorithm>> GetSupportedAlgorithms(IEnumerable<BaseDevice> devices)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ShouldReBenchmarkAlgorithmOnDevice(BaseDevice device, Version benchmarkedPluginVersion, params AlgorithmType[] ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override MinerBase CreateMinerBase()
+        {
+            return new SRBMiner(PluginUUID);
+        }
+    }
+}
