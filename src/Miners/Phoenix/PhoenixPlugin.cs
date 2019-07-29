30b5cb1afe01e5d14ce7d573f422c564610c5fba
Stanko <krsticch@gmail.com>
2019-07-29 15:05:51 +0200

    Phoenix plugin ver bump (split NVIDIA and AMD)

--- src/Miners/Phoenix/PhoenixPlugin.cs
+++ src/Miners/Phoenix/PhoenixPlugin.cs
@@ -32,7 +32,7 @@ namespace Phoenix
 
         public override string PluginUUID => "ac9c763f-c901-41ef-9df1-c80099c9f942";
 
-        public override Version Version => new Version(2, 0);
+        public override Version Version => new Version(2, 1);
         public override string Name => "Phoenix";
 
         public override string Author => "domen.kirnkrefl@nicehash.com";
