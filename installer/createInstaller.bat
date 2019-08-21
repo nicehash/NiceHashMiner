0d995656a9737f6ce3397a938f53b2ad64a46c05
luc1an24 <domen.kirnkrefl@nicehash.com>
2019-08-21 08:32:35 +0200

    add eula and pack ready for github release

--- installer/createInstaller.bat
+++ installer/createInstaller.bat
@@ -10,10 +10,27 @@ if exist .\nhmpacker\_files_to_pack (
 mkdir .\nhmpacker\_files_to_pack\assets
 xcopy /s /i nsis_template .\nhmpacker\_files_to_pack\nsis
 xcopy /s /i .\..\Release .\nhmpacker\_files_to_pack\bins
+copy .\nhm_windows_x.y.z.r-template\EULA.html \nhmpacker\_files_to_pack\assets\EULA.html
+copy .\nhm_windows_x.y.z.r-template\EULA.rtf \nhmpacker\_files_to_pack\assets\EULA.rtf
 
 .\..\src\Tools\InstallerHelper\bin\Release\InstallerHelper.exe %CD%\nhmpacker\_files_to_pack\bins\NiceHashMiner.exe
 copy .\..\src\Tools\InstallerHelper\bin\Release\_files_to_pack\version.txt .\nhmpacker\_files_to_pack\version.txt
 copy .\..\src\Tools\InstallerHelper\bin\Release\_files_to_pack\packageDefsGenerated.nsh .\nhmpacker\_files_to_pack\nsis\include_common\packageDefsGenerated.nsh
 
 .\nhmpacker\nhmpacker.exe
+
+for %%i in (.\nhmpacker\nhm_windows_1.*.exe) do (
+    set "filename=%%~ni"  
+)  
+
+xcopy /s /i .\..\Release %filename%
+copy .\nhm_windows_x.y.z.r-template\EULA.html %filename%
+copy .\nhm_windows_x.y.z.r-template\EULA.rtf %filename% 
+7z a -t7z %filename%.7z .\%filename%\*
+
+copy .\nhmpacker\nhm_*.exe .\
+
+
 rmdir /s /q .\nhmpacker\_files_to_pack
+rmdir /s /q %filename%
+del .\nhmpacker\nhm_*.exe
