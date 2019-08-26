39579f14140c28c5beca2a4cc54f8fbf51f15a32
luc1an24 <domen.kirnkrefl@nicehash.com>
2019-08-26 08:51:31 +0200

    cryptoDredge fixed benchmark + api + update version

---
+++ src/Miners/CryptoDredge/BenchmarkHelpers.cs
@@ -0,0 +1,63 @@
+﻿using MinerPluginToolkitV1;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoDredge
+{
+    public static class BenchmarkHelpers
+    {
+        public static Tuple<double, bool> TryGetHashrate(string s)
+        {
+            var shortenedString = s.Substring(s.IndexOf("Accepted"));
+            var splittedString = shortenedString.Split(':');
+            var afterString = splittedString[2].ToLower().Replace(',', '.');
+            var numString = new string(afterString
+                .ToCharArray()
+                .SkipWhile(c => !char.IsDigit(c))
+                .TakeWhile(c => char.IsDigit(c) || c == '.')
+                .ToArray());
+
+            if (!double.TryParse(numString, NumberStyles.Float, CultureInfo.InvariantCulture, out var hash))
+            {
+                return Tuple.Create(0d, false);
+            }
+
+            var afterNumString = afterString.GetStringAfter(numString);
+            for (var i = 0; i < afterNumString.Length - 1; ++i)
+            {
+                var c = afterNumString[i];
+                if (!Char.IsLetter(c)) continue;
+                var c2 = afterNumString[i + 1];
+
+                foreach (var kvp in _postfixes)
+                {
+                    var postfix = Char.ToLower(kvp.Key);
+                    var mult = kvp.Value;
+                    if (postfix == c && 'h' == c2)
+                    {
+                        var hashrate = hash * mult;
+                        return Tuple.Create(hashrate, true);
+                    }
+                }
+            }
+            return Tuple.Create(hash, true);
+        }
+
+        private static int pow10(int power) => (int)Math.Pow(10, power);
+        private static readonly Dictionary<char, int> _postfixes = new Dictionary<char, int>
+        {
+            {'k', pow10(3)},
+            {'M', pow10(6)},
+            {'G', pow10(9)},
+            {'T', pow10(12)},
+            {'P', pow10(15)},
+            {'E', pow10(15)},
+            {'Z', pow10(21)},
+            {'Y', pow10(24)},
+        };
+    }
+}
