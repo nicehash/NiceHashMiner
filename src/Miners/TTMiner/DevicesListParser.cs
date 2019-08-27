749b921f26689b579f9b61d848b3a2ad9af7c4d7
luc1an24 <domen.kirnkrefl@nicehash.com>
2019-08-27 12:50:25 +0200

    implement device cross refferencing for TTMiner

---
+++ src/Miners/TTMiner/DevicesListParser.cs
@@ -0,0 +1,42 @@
+﻿using NHM.Common.Device;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTMiner
+{
+    internal class DevicesListParser
+    {
+        public static Dictionary<string, int> ParseTTMinerOutput(string output, List<BaseDevice> baseDevices)
+        {
+            var gpus = baseDevices.Where(dev => dev is CUDADevice).Cast<CUDADevice>();
+
+            var mappedDevices = new Dictionary<string, int>();
+
+            var gpuData = output.Substring(output.IndexOf("#"));
+            var lines = gpuData.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            if (lines.Count() != 0)
+            {
+                foreach (var line in lines)
+                {
+                    var cudaID = line.Substring(line.IndexOf('#') + 1, 2);
+                    int.TryParse(cudaID, out var ID);
+                    var pciID = line.Substring(line.IndexOf(':') + 1, 2);
+                    int.TryParse(pciID, out var comparePCIeBusID);
+
+                    foreach (var gpu in gpus)
+                    {
+                        if (gpu.PCIeBusID == comparePCIeBusID)
+                        {
+                            mappedDevices.Add(gpu.UUID, ID);
+                            break;
+                        }
+                    }
+                }
+            }
+            return mappedDevices;
+        }
+    }
+}
