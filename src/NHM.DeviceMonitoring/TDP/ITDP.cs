42e99923ec30830d797aa6535ee53c41c7781034
luc1an24-rig <domen.kirnkrefl@nicehash.com>
2019-09-04 09:27:16 +0200

    implement TDP related interface and enums

---
+++ src/NHM.DeviceMonitoring/TDP/ITDP.cs
@@ -0,0 +1,18 @@
+﻿
+namespace NHM.DeviceMonitoring.TDP
+{
+    public interface ITDP
+    {
+        TDPSettingType SettingType { get; set; }
+
+        // RAW values according to the underlying API
+        double TDPRaw { get; }
+        bool SetTDPRaw(double raw);
+
+        double TDPPercentage { get; }
+        bool SetTDPPercentage(double percentage);
+
+        TDPSimpleType TDPSimple { get; }
+        bool SetTDPSimple(TDPSimpleType level);
+    }
+}
