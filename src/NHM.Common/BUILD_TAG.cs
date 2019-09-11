e303b12c5fa9ba44f4fb2c625abac6adbb2c390d
Stanko <krsticch@gmail.com>
2019-09-11 14:19:27 +0200

    remove production new build tags

--- src/NHM.Common/BUILD_TAG.cs
+++ src/NHM.Common/BUILD_TAG.cs
@@ -7,8 +7,6 @@ namespace NHM.Common
         public static readonly string BuildTag = "TESTNET";
 #elif TESTNETDEV
         public static readonly string BuildTag = "TESTNETDEV";
-#elif PRODUCTION_NEW
-        public static readonly string BuildTag = "PRODUCTION_NEW";
 #else
         public static readonly string BuildTag = "PRODUCTION";
 #endif
