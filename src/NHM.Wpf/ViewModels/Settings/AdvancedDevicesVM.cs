cc39688309bd6ffdebb37561f7b99d42f0b5cdab
Dillon Newell <dillon@newell-labs.ca>
2019-09-18 01:26:33 -0400

    make new settings page for advanced device settings

---
+++ src/NHM.Wpf/ViewModels/Settings/AdvancedDevicesVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHMCore.Configs.Data;
+
+namespace NHM.Wpf.ViewModels.Settings
+{
+    public class AdvancedDevicesVM : SettingsBaseVM
+    {
+        public AdvancedDevicesVM(GeneralConfig confObj)
+            : base(confObj, "Devices")
+        { }
+    }
+}
---
+++ src/NHM.Wpf/Views/Settings/Pages/AdvancedDevicesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace NHM.Wpf.Views.Settings.Pages
+{
+    /// <summary>
+    /// Interaction logic for AdvancedDevicesPage.xaml
+    /// </summary>
+    public partial class AdvancedDevicesPage : UserControl, ISettingsPage
+    {
+        public AdvancedDevicesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
