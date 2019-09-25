6ffc042726f3216d6b9fb3851d47e1c9b0773d14
Dillon Newell <dillon@newell-labs.ca>
2019-09-25 05:05:42 -0400

    add warning for enums used in radio buttons

--- src/NHM.Common/Enums/BenchmarkPerformanceType.cs
+++ src/NHM.Common/Enums/BenchmarkPerformanceType.cs
@@ -1,5 +1,8 @@
 ﻿namespace NHM.Common.Enums
 {
+    // WARNING:
+    // Names of this enum's values are in plaintext in `BenchmarkWindow.xaml` as part of data-binding for radio buttons
+    // When renaming values, make sure to update these references as VS will not catch them
     public enum BenchmarkPerformanceType
     {
         Quick = 0,
