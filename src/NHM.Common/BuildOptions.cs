ae0533dbd1f99802f3f94dd4bd993814ca6c22ee
Stanko <krsticch@gmail.com>
2019-10-07 16:52:32 +0200

    work in progress

---
+++ src/NHM.Common/BuildOptions.cs
@@ -0,0 +1,15 @@
+﻿using NHM.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHM.Common
+{
+    public static class BuildOptions
+    {
+
+        public static BuildTag BuildTag { get; private set; } = BuildTag.PRODUCTION;
+    }
+}
--- src/NHM.Common/DemoUser.cs
+++ src/NHM.Common/DemoUser.cs
@@ -4,11 +4,13 @@ namespace NHM.Common
     public static class DemoUser
     {
 #if TESTNET
-        public static readonly string BTC = "2N6ibfrTwUSSvzAz1esPe1gYULG82asTHiS";
+        public static readonly string _BTC = "2N6ibfrTwUSSvzAz1esPe1gYULG82asTHiS";
 #elif TESTNETDEV
-        public static readonly string BTC = "2N2e2ET1jMY9r5is9KaTKnU3bkCFaYHEEEx"; // TODO
+        public static readonly string _BTC = "2N2e2ET1jMY9r5is9KaTKnU3bkCFaYHEEEx"; // TODO
 #else
-        public static readonly string BTC = "33hGFJZQAfbdzyHGqhJPvZwncDjUBdZqjW";
+        public static readonly string _BTC = "33hGFJZQAfbdzyHGqhJPvZwncDjUBdZqjW";
 #endif
+
+        public static string BTC => _BTC;
     }
 }
