32fc10edf5ebc9c1c08df01e859b4b08df68ff3c
Stanko <krsticch@gmail.com>
2019-11-27 10:37:09 +0100

    XmrStakRx plugin (RandomX)

---
+++ src/Miners/XmrStakRx/Configs/AmdConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XmrStakRx.Configs
+{
+#pragma warning disable IDE1006 // Naming Styles
+
+    [Serializable]
+    public class AmdThreadsConfItem
+    {
+        public int index = 0;
+        public int intensity = 0;
+        public int worksize = 0;
+        public bool affine_to_cpu = false;
+        public int strided_index = 0;
+        public int mem_chunk = 0;
+        public int unroll = 0;
+        public bool comp_mode = false;
+        public int interleave = 0;
+    }
+
+    [Serializable]
+    public class AmdConfig
+    {
+        public List<AmdThreadsConfItem> gpu_threads_conf;
+        public int auto_tune = 0;
+        public int platform_index;
+    }
+#pragma warning restore IDE1006 // Naming Styles
+
+}
---
+++ src/Miners/XmrStakRx/Configs/IXmrStakConfigHandler.cs
@@ -0,0 +1,15 @@
+﻿using NHM.Common.Enums;
+
+namespace XmrStakRx.Configs
+{
+    public interface IXmrStakRxConfigHandler
+    {
+        bool HasConfig(DeviceType deviceType, AlgorithmType algorithmType);
+
+        void SaveMoveConfig(DeviceType deviceType, AlgorithmType algorithmType, string sourcePath);
+
+        CpuConfig GetCpuConfig(AlgorithmType algorithmType);
+        AmdConfig GetAmdConfig(AlgorithmType algorithmType);
+        NvidiaConfig GetNvidiaConfig(AlgorithmType algorithmType);
+    }
+}
