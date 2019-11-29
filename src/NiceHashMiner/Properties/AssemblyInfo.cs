74389053094d349a788f8fc2781b46757ec49b26
Stanko <krsticch@gmail.com>
2019-11-29 11:13:32 +0100

    ver bump

--- src/NiceHashMiner/Properties/AssemblyInfo.cs
+++ src/NiceHashMiner/Properties/AssemblyInfo.cs
@@ -47,5 +47,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.9.2.17")]
-[assembly: AssemblyFileVersion("1.9.2.17")]
+[assembly: AssemblyVersion("1.9.2.18")]
+[assembly: AssemblyFileVersion("1.9.2.18")]
