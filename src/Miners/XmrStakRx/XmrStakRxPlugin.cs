03824121d294c965b7fba09a2894a0baa2343f87
Stanko <krsticch@gmail.com>
2019-12-02 12:25:23 +0100

    XmrStakRxPlugin remove '--noTest ' from MiningCreateCommandLine

--- src/Miners/XmrStakRx/XmrStakRxPlugin.cs
+++ src/Miners/XmrStakRx/XmrStakRxPlugin.cs
@@ -47,7 +47,7 @@ namespace XmrStakRx
 
         public override string PluginUUID => "4aec5ec0-10f8-11ea-bad3-8dea21141bbb";
 
-        public override Version Version => new Version(4, 0);
+        public override Version Version => new Version(4, 1);
         public override string Name => "XmrStakRx";
 
         public override string Author => "info@nicehash.com";
