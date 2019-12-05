7e3b0449e96866fd12cb696670795ef7c285a43f
Stanko <krsticch@gmail.com>
2019-12-05 16:10:54 +0100

    Settings page make it resizable

--- src/NHM.Wpf/Views/SettingsNew/Settings.xaml.cs
+++ src/NHM.Wpf/Views/SettingsNew/Settings.xaml.cs
@@ -1,11 +1,6 @@
-﻿using NHM.Wpf.ViewModels;
-using NHMCore.Configs;
-using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using NHMCore.Configs;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace NHM.Wpf.Views.SettingsNew
 {
@@ -14,49 +9,40 @@ namespace NHM.Wpf.Views.SettingsNew
     /// </summary>
     public partial class Settings : UserControl
     {
-        private static bool _isGeneral = true;
+        private bool _isGeneral = true;
 
         public Settings()
         {
             InitializeComponent();
-            OnScreenChange();
+            OnScreenChange(_isGeneral);
         }
 
-        protected void OnScreenChange()
+        protected void OnScreenChange(bool isGeneral)
         {
-            if (_isGeneral)
+            if (isGeneral)
             {
-                grid_SettingsGeneral.Visibility = Visibility.Visible;
-                spt_general.Visibility = Visibility.Visible;
-                btn_SettingsGeneral.Foreground = (Brush)FindResource("Brushes.Dark.Basic.MainColor");
-
-                grid_SettingsAdvanced.Visibility = Visibility.Hidden;
-                spt_advanced.Visibility = Visibility.Hidden;
-                btn_SettingsAdvanced.Foreground = (Brush)FindResource("Gray2ColorBrush");
+                GeneralButton.IsChecked = true;
+                AdvancedButton.IsChecked = false;
+                GeneralTab.IsSelected = true;
             }
             else
             {
-                grid_SettingsGeneral.Visibility = Visibility.Hidden;
-                spt_general.Visibility = Visibility.Hidden;
-                btn_SettingsGeneral.Foreground = (Brush)FindResource("Gray2ColorBrush");
-
-
-                grid_SettingsAdvanced.Visibility = Visibility.Visible;
-                spt_advanced.Visibility = Visibility.Visible;
-                btn_SettingsAdvanced.Foreground = (Brush)FindResource("Brushes.Dark.Basic.MainColor");
+                GeneralButton.IsChecked = false;
+                AdvancedButton.IsChecked = true;
+                AdvancedTab.IsSelected = true;
             }
         }
 
         private void Btn_GeneralSettings_Click(object sender, RoutedEventArgs e)
         {
             _isGeneral = true;
-            OnScreenChange();
+            OnScreenChange(_isGeneral);
         }
 
         private void Btn_AdvancedSettings_Click(object sender, RoutedEventArgs e)
         {
             _isGeneral = false;
-            OnScreenChange();
+            OnScreenChange(_isGeneral);
         }
 
         private void Btn_default_Click(object sender, RoutedEventArgs e)
