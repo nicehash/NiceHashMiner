dacdd2af91fa56c4d75a691d1919c9044b97066a
Stanko <krsticch@gmail.com>
2019-12-17 14:04:50 +0100

    core add notifications manager

---
+++ src/NHMCore/Notifications/NotificationsManager.cs
@@ -0,0 +1,18 @@
+﻿using NHM.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHMCore.Notifications
+{
+    public class NotificationsManager : NotifyChangedBase
+    {
+        public static NotificationsManager Instance { get; } = new NotificationsManager();
+
+        private NotificationsManager()
+        {}
+        // TODO add notifications list
+    }
+}
