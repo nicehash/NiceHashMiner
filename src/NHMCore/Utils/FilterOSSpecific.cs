5d1aed2d9ad3c18db191a114d523e12eb9685123
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-01-17 15:10:52 +0100

    implemented GetWindowsVersion

---
+++ src/NHMCore/Utils/FilterOSSpecific.cs
@@ -0,0 +1,49 @@
+﻿using NHM.Common;
+using System;
+using System.Collections.Generic;
+using System.Management;
+
+namespace NHMCore.Utils
+{
+    public static class FilterOSSpecific
+    {
+        public static void GetWindowsVersion()
+        {
+            Logger.Info("NHM", "Get OS version START");
+            var OSName = "";
+            var BuildNumber = 0;
+            try
+            {
+                using (var searcher = new ManagementObjectSearcher("root\\CIMV2", $"SELECT Caption,BuildNumber FROM Win32_OperatingSystem"))
+                using (var query = searcher.Get())
+                {
+                    foreach (var item in query)
+                    {
+                        if (!(item is ManagementObject mo)) continue;
+                        OSName = item.GetPropertyValue("Caption").ToString();
+                        BuildNumber = Convert.ToInt32(item.GetPropertyValue("BuildNumber"));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error("NHM", $"Get OS version error: {e.Message}");
+            }
+
+            SystemVersion.FullName = OSName;
+            SystemVersion.BuildNumber = BuildNumber;
+
+            //win 7 builds
+            if (BuildNumber == 7600 || BuildNumber == 7601)
+            {
+                SystemVersion.OsVersion = 7;
+            }
+            else if (BuildNumber > 10000) //windows 10 builds
+            {
+                SystemVersion.OsVersion = 10;
+            }
+
+            Logger.Info("NHM", "Get OS version END");
+        }
+    }
+}
