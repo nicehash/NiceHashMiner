c6674f4808ef857442cdeabcaaca6078ac02d491
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-01-24 15:14:03 +0100

    copy notifications to notification bar in menu

---
+++ src/NiceHashMiner/Views/Notifications/NotificationItem.xaml.cs
@@ -0,0 +1,49 @@
+﻿using NHMCore.Notifications;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace NiceHashMiner.Views.Notifications
+{
+    /// <summary>
+    /// Interaction logic for NotificationItem.xaml
+    /// </summary>
+    public partial class NotificationItem : UserControl
+    {
+
+        private Notification _notification;
+        public NotificationItem()
+        {
+            InitializeComponent();
+            DataContextChanged += NotificationItemItem_DataContextChanged;
+        }
+
+        private void NotificationItemItem_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is Notification notification)
+            {
+                _notification  = notification;
+                var baseAction = _notification.Actions.FirstOrDefault();
+                if (baseAction is NotificationAction action)
+                {
+                    ActionButton.Content = action.Info;
+                    ActionButton.Click += (s, be) => action.Action?.Invoke();
+                    ActionButton.Visibility = Visibility.Visible;
+                }
+                return;
+            }
+            throw new Exception("unsupported datacontext type");
+        }
+
+        private void RemoveNotification(object sender, RoutedEventArgs e)
+        {
+            _notification.RemoveNotification();
+        }
+
+        private void ExecuteNotificationAction(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- src/NiceHashMiner/Views/Notifications/Notifications.xaml.cs
+++ src/NiceHashMiner/Views/Notifications/Notifications.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace NiceHashMiner.Views.Notifications
 {
@@ -23,6 +10,12 @@ namespace NiceHashMiner.Views.Notifications
         public Notifications()
         {
             InitializeComponent();
+            //checkItems();
+        }
+
+        private void checkItems()
+        {
+            if (ic_NotificationsList.Items.Count == 0) ic_NotificationsList.Items.Add(new Label() { Content = "No new notifications" });
         }
     }
 }
