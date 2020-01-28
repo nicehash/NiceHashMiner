32481af85322b0fed5c999fd4c80683c0eb1a62d
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-01-28 12:47:29 +0100

    show new rectangle with new notification

---
+++ src/NiceHashMiner/Views/Notifications/Converters/NewVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using NHMCore.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NiceHashMiner.Views.Notifications.Converters
+{
+    public class NewVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool notificationNew)
+            {
+                if(notificationNew == true) return Visibility.Visible;
+                return Visibility.Hidden;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
