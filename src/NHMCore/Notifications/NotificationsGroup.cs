0920cf25e37a01471194783f47693a4af977c9e6
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-02-12 13:24:02 +0100

    allow only 1 notification per group

--- src/NHMCore/Notifications/NotificationsGroup.cs
+++ src/NHMCore/Notifications/NotificationsGroup.cs
@@ -11,5 +11,36 @@ namespace NHMCore.Notifications
         Misc,
         Market,
         Profit,
+        MonitoringNvidiaElevate,
+        EthlargementElevate,
+        ConnectionLost,
+        NoEnabledDevice,
+        DemoMining,
+        NoSma,
+        [Obsolete]
+        NoDeviceSelectedBenchmark,
+        [Obsolete]
+        NothingToBenchmark,
+        FailedBenchmarks,
+        NoSupportedDevices,
+        MissingMiners,
+        MissingMinerBins,
+        [Obsolete]
+        FailedVideoController,
+        [Obsolete]
+        WmiEnabled,
+        [Obsolete]
+        Net45,
+        [Obsolete]
+        BitOS64,
+        NhmUpdate,
+        PluginUpdate,
+        NvidiaDCH,
+        WindowsDefenderException,
+        ComputeModeAMD,
+        LargePages,
+        VirtualMemory,
+        NVMLFallbackFail,
+        OpenClFallback
     }
 }
