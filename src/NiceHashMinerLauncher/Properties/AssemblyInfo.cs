a129066ac1b482c5e2c5cff1f589652e22b8604b
Stanko <krsticch@gmail.com>
2020-02-14 12:36:19 +0100

    Version bump

--- src/NiceHashMinerLauncher/Properties/AssemblyInfo.cs
+++ src/NiceHashMinerLauncher/Properties/AssemblyInfo.cs
@@ -65,5 +65,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.0.0.4")]
-[assembly: AssemblyFileVersion("3.0.0.4")]
+[assembly: AssemblyVersion("3.0.0.5")]
+[assembly: AssemblyFileVersion("3.0.0.5")]
