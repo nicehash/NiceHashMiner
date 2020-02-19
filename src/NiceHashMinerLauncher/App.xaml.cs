43551704f5e2628fd1bc35e47e00254ac72ef926
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-02-19 11:06:46 +0100

    delete plugins_packages on zip update

--- src/NiceHashMinerLauncher/App.xaml.cs
+++ src/NiceHashMinerLauncher/App.xaml.cs
@@ -203,6 +203,7 @@ namespace NiceHashMiner
                 //await Task.Delay(5000);
                 if (isZip)
                 {
+                    Directory.Delete(GetRootPath("plugins_packages"), true);
                     var progWindow = new UpdateProgress();
                     progWindow.Show();
                     var isOk = await UnzipFileAsync(updaterFile, GetRootPath(), progWindow.Progress, CancellationToken.None);
