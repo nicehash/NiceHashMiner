e9efc8819ea583908ec90ddcf8a04356d452c0d6
Stanko <krsticch@gmail.com>
2020-03-02 14:32:28 +0100

    NBMinerPlugin update bins

--- src/Miners/NBMiner/NBMinerPlugin.cs
+++ src/Miners/NBMiner/NBMinerPlugin.cs
@@ -26,11 +26,11 @@ namespace NBMiner
             // https://github.com/NebuTech/NBMiner/releases/ 
             MinersBinsUrlsSettings = new MinersBinsUrlsSettings
             {
-                BinVersion = "v27.2",
+                BinVersion = "v27.4",
                 ExePath = new List<string> { "NBMiner_Win", "nbminer.exe" },
                 Urls = new List<string>
                 {
-                    "https://github.com/NebuTech/NBMiner/releases/download/v27.2/NBMiner_27.2_Win.zip", // original
+                    "https://github.com/NebuTech/NBMiner/releases/download/v27.4/NBMiner_27.4_Win.zip", // original
                 }
             };
             PluginMetaInfo = new PluginMetaInfo
@@ -42,7 +42,7 @@ namespace NBMiner
 
         public override string PluginUUID => "6c07f7a0-7237-11e9-b20c-f9f12eb6d835";
 
-        public override Version Version => new Version(8, 1);
+        public override Version Version => new Version(8, 2);
         public override string Name => "NBMiner";
 
         public override string Author => "info@nicehash.com";
