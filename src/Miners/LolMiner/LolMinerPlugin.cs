6984b405b96a61b2124d25bedbaabeb9412895c6
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-03-18 12:39:59 +0100

    add zhash to lolminer for amd + ver bump

--- src/Miners/LolMiner/LolMinerPlugin.cs
+++ src/Miners/LolMiner/LolMinerPlugin.cs
@@ -37,7 +37,7 @@ namespace LolMiner
             };
         }
 
-        public override Version Version => new Version(8, 1);
+        public override Version Version => new Version(8, 2);
 
         public override string Name => "lolMiner";
 
