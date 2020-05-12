6dc0add8ffffc57c6c1e76c75b201d7650e9b5a9
Stanko <krsticch@gmail.com>
2020-05-12 16:19:05 +0200

    delete unused code

--- src/NHM.MinerPluginToolkitV1/DevicesCrossReferenceHelpers.cs
+++ src/NHM.MinerPluginToolkitV1/DevicesCrossReferenceHelpers.cs
@@ -18,54 +18,6 @@ namespace NHM.MinerPluginToolkitV1
         /// <summary>
         /// MinerOutput creates new process and gets its StandardOutput
         /// </summary>
-        //public static async Task<string> MinerOutput(string path, string arguments, int timeoutMilliseconds = 30 * 1000)
-        //{
-        //    string output = "";
-        //    try
-        //    {
-        //        var startInfo = new ProcessStartInfo
-        //        {
-        //            FileName = path,
-        //            Arguments = arguments,
-        //            CreateNoWindow = true,
-        //            UseShellExecute = false,
-        //            RedirectStandardOutput = true,
-        //        };
-
-        //        using (var getDevicesHandle = new Process { StartInfo = startInfo, EnableRaisingEvents = true })
-        //        using (var ct = new CancellationTokenSource(timeoutMilliseconds))
-        //        {
-        //            getDevicesHandle.Start();
-        //            Action<string> getDevicesHandleStop = (string stopFrom) => {
-        //                try
-        //                {
-        //                    var isRunning = !getDevicesHandle?.HasExited ?? false;
-        //                    if (!isRunning) return;
-        //                    getDevicesHandle.CloseMainWindow();
-        //                    var hasExited = getDevicesHandle?.WaitForExit(1000) ?? false;
-        //                    if (!hasExited) getDevicesHandle.Kill();
-        //                }
-        //                catch (Exception e)
-        //                {
-        //                    Logger.Error("DeviceCrossReference", $"Unable to get DevicesHandle: {e.Message}");
-        //                }
-        //            };
-        //            ct.Token.Register(() => getDevicesHandleStop("from cancel token"));
-        //            output = await getDevicesHandle.StandardOutput.ReadToEndAsync();
-        //            getDevicesHandleStop("after read to end");
-        //        }
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Logger.Error("DevicesCrossReferenceHelpers", $"Error occured while getting miner output: {e.Message}");
-        //        return "";
-        //    }
-        //    return output;
-        //}
-
-        /// <summary>
-        /// MinerOutput creates new process and gets its StandardOutput
-        /// </summary>
         public static async Task<string> MinerOutput(string path, string arguments, int timeoutMilliseconds = 30 * 1000)
         {
             string output = "";
