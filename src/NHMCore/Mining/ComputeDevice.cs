a00e6802771b09c4ceb91e95af2bdade47a4ef87
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-06-22 15:04:56 +0200

    enable default val set

--- src/NHMCore/Mining/ComputeDevice.cs
+++ src/NHMCore/Mining/ComputeDevice.cs
@@ -353,8 +353,7 @@ namespace NHMCore.Mining
             //Enabled = config.Enabled;
             Enabled = config.Enabled;
             MinimumProfit = config.MinimumProfit;
-
-            /*
+           
             if (!DeviceMonitorManager.DisableDevicePowerModeSettings)
             {
                 var tdpSimpleDefault = TDPSimpleType.HIGH;
@@ -397,7 +396,6 @@ namespace NHMCore.Mining
                     tdpDefault.SetTDPSimple(tdpSimpleDefault); // set default high
                 }
             }
-            */
 
             if (config.PluginAlgorithmSettings == null) return;
             PluginAlgorithmSettings = config.PluginAlgorithmSettings;
