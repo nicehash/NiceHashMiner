017fc81c8266b078605a1978475f8a3f51c19593
Stanko <krsticch@gmail.com>
2020-06-27 09:49:41 +0200

    new lolminer beav3 plugin

---
+++ src/Miners/LolMiner_BeamV3/LolMiner.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json;
+using NHM.Common;
+using NHM.Common.Enums;
+using NHM.MinerPlugin;
+using NHM.MinerPluginToolkitV1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LolMiner_BeamV3
+{
+    public class LolMiner : MinerBase
+    {
+        private string _devices;
+        private int _apiPort;
+        private double DevFee = 1d;
+
+        private string _disableWatchdogParam = "--disablewatchdog 1";
+
+        // the order of intializing devices is the order how the API responds
+        private Dictionary<int, string> _initOrderMirrorApiOrderUUIDs = new Dictionary<int, string>();
+        protected Dictionary<string, int> _mappedIDs;
+
+        private readonly HttpClient _http = new HttpClient();
+
+        public LolMiner (string uuid, Dictionary<string, int> mappedIDs) : base(uuid)
+        {
+            _mappedIDs = mappedIDs;
+        }
+
+        protected virtual string AlgorithmName(AlgorithmType algorithmType) => PluginSupportedAlgorithms.AlgorithmName(algorithmType);
+
+        public async override Task<ApiData> GetMinerStatsDataAsync()
+        {
+            var ad = new ApiData();
+            try
+            {
+                var summaryApiResult = await _http.GetStringAsync($"http://127.0.0.1:{_apiPort}/summary");
+                ad.ApiResponse = summaryApiResult;
+                var summary = JsonConvert.DeserializeObject<ApiJsonResponse>(summaryApiResult);
+                var perDeviceSpeedInfo = new Dictionary<string, IReadOnlyList<(AlgorithmType type, double speed)>>();
+                var totalSpeed = summary.Session.Performance_Summary;
+
+                var totalPowerUsage = 0;
+                var perDevicePowerInfo = new Dictionary<string, int>();
+
+                var apiDevices = summary.GPUs;
+
+                foreach(var pair in _miningPairs)
+                {
+                    var gpuUUID = pair.Device.UUID;
+                    var gpuID = _mappedIDs[gpuUUID];
+                    var currentStats = summary.GPUs.Where(devStats => devStats.Index == gpuID).FirstOrDefault();
+                    if (currentStats == null) continue;
+                    perDeviceSpeedInfo.Add(gpuUUID, new List<(AlgorithmType type, double speed)>() { (_algorithmType, currentStats.Performance * (1 - DevFee * 0.01)) });
+                }
+
+                ad.AlgorithmSpeedsPerDevice = perDeviceSpeedInfo;
+                ad.PowerUsageTotal = totalPowerUsage;
+                ad.PowerUsagePerDevice = perDevicePowerInfo;
+            }
+            catch (Exception e)
+            {
+                Logger.Error(_logGroup, $"Error occured while getting API stats: {e.Message}");
+            }
+
+            return ad;
+        }
+
+        protected override IEnumerable<MiningPair> GetSortedMiningPairs(IEnumerable<MiningPair> miningPairs)
+        {
+            var pairsList = miningPairs.ToList();
+            // sort by mapped ids
+            pairsList.Sort((a, b) => _mappedIDs[a.Device.UUID].CompareTo(_mappedIDs[b.Device.UUID]));
+            return pairsList;
+        }
+
+        protected override void Init()
+        {
+            _devices = string.Join(",", _miningPairs.Select(p => _mappedIDs[p.Device.UUID]));
+
+            // ???????? GetSortedMiningPairs is now sorted so this thing probably makes no sense anymore
+            var miningPairs = _miningPairs.ToList();
+            for (int i = 0; i < miningPairs.Count; i++)
+            {
+                _initOrderMirrorApiOrderUUIDs[i] = miningPairs[i].Device.UUID;
+            }
+            // if ELP contains watchdog remove default wd-param
+            if (_extraLaunchParameters.Contains("--disablewatchdog"))
+            {
+                _disableWatchdogParam = "";
+            }
+        }
+
+        protected override string MiningCreateCommandLine()
+        {
+            // API port function might be blocking
+            _apiPort = GetAvaliablePort();
+            // instant non blocking
+            var urlWithPort = StratumServiceHelpers.GetLocationUrl(_algorithmType, _miningLocation, NhmConectionType.STRATUM_TCP);
+            var split = urlWithPort.Split(':');
+            var url = split[1].Substring(2, split[1].Length - 2);
+            var port = split[2];
+
+            var algo = AlgorithmName(_algorithmType);
+            //--disablewatchdog 1
+            var commandLine = $"--algo {algo} --pool {url}:{port} --user {_username} --tls 0 --apiport {_apiPort} {_disableWatchdogParam} --devices {_devices} {_extraLaunchParameters}";
+            return commandLine;
+        }
+    }
+}
---
+++ src/Miners/LolMiner_BeamV3/LolMinerPlugin.PluginSupportedAlgorithms.cs
@@ -0,0 +1,54 @@
+﻿using NHM.MinerPluginToolkitV1.Configs;
+using NHM.Common.Enums;
+using System.Collections.Generic;
+
+using SAS = NHM.MinerPluginToolkitV1.Configs.PluginSupportedAlgorithmsSettings.SupportedAlgorithmSettings;
+
+namespace LolMiner_BeamV3
+{
+    public partial class LolMinerPlugin
+    {
+        const ulong AMD_8GBMemory = 7UL << 30; // 7GB but really 8GB
+        const ulong AMD_6GBMemory = 5UL << 30; // 5GB but really 6GB
+        internal static List<SAS> SupportedAMDAlgos()
+        {
+            return new List<SAS>
+                    {
+                        new SAS(AlgorithmType.BeamV3) { NonDefaultRAMLimit = AMD_6GBMemory },
+                    };
+        }
+
+        // NVIDIA OpenCL backend is not really that stable
+        internal static List<SAS> SupportedNVIDIAOpenCLAlgos(bool enabled = false)
+        {
+            return new List<SAS>
+                    {
+                        new SAS(AlgorithmType.GrinCuckarood29) {Enabled = enabled },
+                        new SAS(AlgorithmType.GrinCuckatoo31) {Enabled = enabled },
+                        new SAS(AlgorithmType.BeamV2) {Enabled = enabled },
+                    };
+        }
+
+        protected override PluginSupportedAlgorithmsSettings DefaultPluginSupportedAlgorithmsSettings => new PluginSupportedAlgorithmsSettings
+        {
+            // fixed fee
+            DefaultFee = 1.0,
+            Algorithms = new Dictionary<DeviceType, List<SAS>>
+            {
+                // don't use NVIDIA OpenCL backend
+                //{
+                //    DeviceType.NVIDIA,
+                //    SupportedNVIDIAOpenCLAlgos(false) // dsable NVIDIA by default
+                //},
+                {
+                    DeviceType.AMD,
+                    SupportedAMDAlgos()
+                },
+            },
+            AlgorithmNames = new Dictionary<AlgorithmType, string>
+            {
+                { AlgorithmType.BeamV3, "BEAM-III" }, // -a
+            }
+        };
+    }
+}
