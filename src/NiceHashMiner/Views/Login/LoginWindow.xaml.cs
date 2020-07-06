03442887e3a176a8c3b7b0deb2e089b75d0970d1
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-07-06 15:34:37 +0200

    working qr with icon for testnet-dev

--- src/NiceHashMiner/Views/Login/LoginWindow.xaml.cs
+++ src/NiceHashMiner/Views/Login/LoginWindow.xaml.cs
@@ -14,10 +14,8 @@ using System.IO;
 using System.Windows.Media.Imaging;
 using System.Drawing.Imaging;
 using System.Net.Http;
-using System.Collections.Generic;
 using NHMCore;
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Text;
 using Newtonsoft.Json;
@@ -30,15 +28,13 @@ namespace NiceHashMiner.Views.Login
     public partial class LoginWindow : BaseDialogWindow
     {
         private static readonly HttpClient client = new HttpClient();
-        private Timer _evalTimer;
-        private object _lock = new object();
 
         public LoginWindow()
         {
             InitializeComponent();
             HideIconAndTitle = true;
             WindowUtils.Translate(this);
-            ProcessQRCode();           
+            _ = ProcessQRCode();           
         }
 
         private void CheckBoxMode_Checked(object sender, RoutedEventArgs e)
@@ -70,7 +66,7 @@ namespace NiceHashMiner.Views.Login
             browser.ShowDialog();
         }
 
-        private void ProcessQRCode()
+        private async Task ProcessQRCode()
         {
             var uuid = Guid.NewGuid().ToString();
             var rigID = ApplicationStateManager.RigID();
@@ -78,8 +74,8 @@ namespace NiceHashMiner.Views.Login
             var requestBody = "{\"qrId\":\""+ uuid + "\", \"rigId\":\"" + rigID + "\"}";
             var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
 
-            //var response = client.PostAsync("https://api2.nicehash.com/api/v2/organization/nhmqr", content).Result;
-            var response = client.PostAsync("https://api-test-dev.nicehash.com/api/v2/organization/nhmqr", content).Result;
+            //var response = await client.PostAsync("https://api2.nicehash.com/api/v2/organization/nhmqr", content);
+            var response = await client.PostAsync("https://api-test-dev.nicehash.com/api/v2/organization/nhmqr", content);
 
             var responseString = response.Content.ReadAsStringAsync();
 
@@ -87,39 +83,35 @@ namespace NiceHashMiner.Views.Login
             CreateQRCode(uuid);
 
             //if all ok start timer to poll
-            _evalTimer = new Timer((s) => { Dispatcher.Invoke(() =>
+            while (true)
             {
-                using (var tryLock = new TryLock(_lock))
+                await Task.Delay(2000);
+                try
                 {
-                    if (!tryLock.HasAcquiredLock) return;
-                    try
+                    //var resp = client.GetAsync($"https://api2.nicehash.com/api/v2/organization/nhmqr/{uuid}").Result;
+                    var resp = client.GetAsync($"https://api-test-dev.nicehash.com/api/v2/organization/nhmqr/{uuid}").Result;
+                    if (resp.IsSuccessStatusCode)
                     {
-                        var resp = client.GetAsync($"https://api-test-dev.nicehash.com/api/v2/organization/nhmqr/{uuid}").Result;
-                        if (resp.IsSuccessStatusCode)
+                        var contentString = resp.Content.ReadAsStringAsync().Result;
+                        if (!string.IsNullOrEmpty(contentString))
                         {
-                            if (!string.IsNullOrEmpty(resp.Content.ToString()))
+                            var btcResp = JsonConvert.DeserializeObject<BtcResponse>(contentString);
+                            if (btcResp.btc != null)
                             {
-                                Console.WriteLine("CHECK THIS OUT: ", resp.Content);
-                                var btcResp = JsonConvert.DeserializeObject<BtcResponse>(resp.Content.ToString());
-                                if (btcResp.btc != null)
+                                if (CredentialValidators.ValidateBitcoinAddress(btcResp.btc))
                                 {
-                                    var result = ApplicationStateManager.SetBTCIfValidOrDifferent(btcResp.btc).Result;
-                                    if (result == ApplicationStateManager.SetResult.CHANGED)
-                                    {
-                                        _evalTimer.Dispose();
-                                        Close();
-                                    }
-                                }
-
+                                    CredentialsSettings.Instance.SetBitcoinAddress(btcResp.btc);
+                                    Close();
+                                }                             
                             }
                         }
                     }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
                 }
-            }); }, null, 100, 1000);
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }    
         }
 
         private void CreateQRCode(string uuid)
@@ -138,6 +130,7 @@ namespace NiceHashMiner.Views.Login
             bw.Options = encOptions;
             bw.Format = BarcodeFormat.QR_CODE;
 
+            /*
             //convert drawing group to bitmap
             var drawingLogo = Application.Current.FindResource("LoginQRCircleLight") as Drawing;
             var drawingLogoImage = new DrawingImage(drawingLogo);
@@ -154,29 +147,39 @@ namespace NiceHashMiner.Views.Login
                 }
             }
             // end of converting
+            */
 
             var bm = bw.Write(uuid);
-            int deltaHeigth = bm.Height - overlay.Height;
-            int deltaWidth = bm.Width - overlay.Width;
-
-            var g = Graphics.FromImage(overlay);
-            g.DrawImage(overlay, new System.Drawing.Point(deltaWidth / 2, deltaHeigth / 2));
-
-            //bmp to bmpimg
-            BitmapImage bitmapImage;
-            using (var memory = new MemoryStream())
+            try
             {
-                bm.Save(memory, ImageFormat.Png);
-                memory.Position = 0;
-                bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.StreamSource = memory;
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.EndInit();
+                var overlay = new Bitmap("../Resources/logo.png");
+                var g = Graphics.FromImage(bm);
+                var x = (bm.Width - overlay.Width) / 2;
+                var y = (bm.Height - overlay.Height) / 2;
+                g.FillRectangle(new SolidBrush(System.Drawing.Color.White), x, y, overlay.Width, overlay.Height);
+                g.DrawImage(overlay, new System.Drawing.Point(x, y));
+
+                //bmp to bmpimg
+                BitmapImage bitmapImage;
+                using (var memory = new MemoryStream())
+                {
+                    bm.Save(memory, ImageFormat.Png);
+                    memory.Position = 0;
+                    bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.StreamSource = memory;
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.EndInit();
+                }
+                // end of bmp to bmpimg
+
+                var brush = new ImageBrush(bitmapImage);
+                rect_qrCode.Fill = brush;
             }
-            // end of bmp to bmpimg
-            var brush = new ImageBrush(bitmapImage);
-            rect_qrCode.Fill = brush;
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }           
         }
 
         private BitmapSource ToBitmapSource(DrawingImage source)
@@ -189,29 +192,6 @@ namespace NiceHashMiner.Views.Login
             RenderTargetBitmap bmp = new RenderTargetBitmap((int)source.Width, (int)source.Height, 96, 96, PixelFormats.Pbgra32);
             bmp.Render(drawingVisual);
             return bmp;
-        }
-
-        internal class TryLock : IDisposable
-        {
-            private object locked;
-            public bool HasAcquiredLock { get; private set; }
-            public TryLock(object obj)
-            {
-                if (Monitor.TryEnter(obj))
-                {
-                    HasAcquiredLock = true;
-                    locked = obj;
-                }
-            }
-            public void Dispose()
-            {
-                if (HasAcquiredLock)
-                {
-                    Monitor.Exit(locked);
-                    locked = null;
-                    HasAcquiredLock = false;
-                }
-            }
         }
 
         [Serializable]
