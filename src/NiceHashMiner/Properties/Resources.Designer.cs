42a88c34a95e713c4359f35292a0c124c440e063
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-07-27 09:36:20 +0200

    move logo icons to resources

--- src/NiceHashMiner/Properties/Resources.Designer.cs
+++ src/NiceHashMiner/Properties/Resources.Designer.cs
@@ -69,5 +69,25 @@ namespace NiceHashMiner.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap logoDark32 {
+            get {
+                object obj = ResourceManager.GetObject("logoDark32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap logoLight32 {
+            get {
+                object obj = ResourceManager.GetObject("logoLight32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
