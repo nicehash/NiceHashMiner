1327cfe1c48104656d53350253cf44fdb22b5dbc
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-08-17 15:08:02 +0200

    dagger 5gb algo + mark GrinCuckaroo29 obsolete

--- src/Miners/Phoenix/PhoenixPlugin.cs
+++ src/Miners/Phoenix/PhoenixPlugin.cs
@@ -42,7 +42,7 @@ namespace Phoenix
 
         public override string PluginUUID => "fa369d10-94eb-11ea-a64d-17be303ea466";
 
-        public override Version Version => new Version(11, 3);
+        public override Version Version => new Version(11, 4);
         public override string Name => "Phoenix";
 
         public override string Author => "info@nicehash.com";
