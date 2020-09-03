79683921ee2e48ce29ddcb1d849e01288f76d2c8
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-09-03 15:12:48 +0200

    remove x16r from ccminer

--- src/Miners/CCMinerTpruvot/CCMinerTpruvotPlugin.PluginSupportedAlgorithms.cs
+++ src/Miners/CCMinerTpruvot/CCMinerTpruvotPlugin.PluginSupportedAlgorithms.cs
@@ -17,15 +17,13 @@ namespace CCMinerTpruvot
                     DeviceType.NVIDIA,
                     new List<SAS>
                     {
-                        new SAS(AlgorithmType.X16R) { Enabled=false },
                         new SAS(AlgorithmType.Lyra2REv3)
                     }
                 }
             },
             AlgorithmNames = new Dictionary<AlgorithmType, string>
             {
-                { AlgorithmType.X16R, "x16r" },
-                { AlgorithmType.Lyra2REv3, "lyra2v3" },
+                { AlgorithmType.Lyra2REv3, "lyra2v3" }
             }
         };
     }
