dbcc5be7fae38fbf8e0bc2d890ff9f1cca3c4666
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-09-03 15:26:02 +0200

    remove x16r from zenemy

--- src/Miners/ZEnemy/ZEnemyPlugin.cs
+++ src/Miners/ZEnemy/ZEnemyPlugin.cs
@@ -40,7 +40,7 @@ namespace ZEnemy
             };
         }
 
-        public override Version Version => new Version(11, 2);
+        public override Version Version => new Version(11, 3);
 
         public override string Name => "ZEnemy";
 
