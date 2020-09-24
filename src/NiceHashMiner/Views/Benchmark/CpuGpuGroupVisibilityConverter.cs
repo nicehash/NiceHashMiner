0e56dbee3f495ddecb896bc5559d235463ec94e1
Stanko <krsticch@gmail.com>
2020-09-24 14:30:16 +0200

    Benchmarks hide CPU|GPU group if empty

---
+++ src/NiceHashMiner/Views/Benchmark/CpuGpuGroupVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NiceHashMiner.Views.Benchmark
+{
+    class CpuGpuGroupVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int count)
+            {
+                if (count == 0) return Visibility.Hidden;
+                return Visibility.Visible;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
