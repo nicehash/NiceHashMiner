4bf8695bdd36df0ed7dad225ad14ffc1e9a2a7c4
Stanko <krsticch@gmail.com>
2020-09-25 14:33:51 +0200

    update device detection libs

--- pre_compiled_libs/README.md
+++ pre_compiled_libs/README.md
@@ -1,1 +1,1 @@
-device_detection_x64 from https://github.com/nicehash/DeviceDetection/releases/tag/v1.4
+device_detection_x64 from https://github.com/nicehash/DeviceDetection/releases/tag/v1.6
--- pre_compiled_libs/device_detection_x64/test.bat
+++ pre_compiled_libs/device_detection_x64/test.bat
@@ -2,20 +2,12 @@ echo "cuda_device_detection.exe -p" > testOutput.txt
 .\cuda_device_detection.exe -p >> testOutput.txt
 echo. >> testOutput.txt
 
-echo "cuda_device_detection.exe -p -nvmlFallback" >> testOutput.txt
-.\cuda_device_detection.exe -p -nvmlFallback >> testOutput.txt
-echo. >> testOutput.txt
-
 echo "opencl_device_detection.exe -p" >> testOutput.txt
 .\opencl_device_detection.exe -p >> testOutput.txt
 echo. >> testOutput.txt
 
 echo "DeviceDetectionPrinter.exe cuda -p" >> testOutput.txt
 .\DeviceDetectionPrinter.exe cuda -p >> testOutput.txt
-echo. >> testOutput.txt
-
-echo "DeviceDetectionPrinter.exe cuda -p -nvmlFallback" >> testOutput.txt
-.\DeviceDetectionPrinter.exe cuda -p -nvmlFallback >> testOutput.txt
 echo. >> testOutput.txt
 
 echo "DeviceDetectionPrinter.exe ocl -p" >> testOutput.txt
@@ -36,10 +28,6 @@ echo. >> testOutput.txt
 
 echo "DeviceDetectionPrinter.exe cuda -p" >> testOutput.txt
 .\DeviceDetectionPrinter.exe cuda -p >> testOutput.txt
-echo. >> testOutput.txt
-
-echo "DeviceDetectionPrinter.exe cuda -p -nvmlFallback" >> testOutput.txt
-.\DeviceDetectionPrinter.exe cuda -p -nvmlFallback >> testOutput.txt
 echo. >> testOutput.txt
 
 echo "DeviceDetectionPrinter.exe ocl -p" >> testOutput.txt
