9eb9ad4b91ae5129ff3722f1ff445515007d7313
luc1an24 <domen.kirnkrefl@nicehash.com>
2020-11-10 13:28:42 +0100

    add nhmws message types

---
+++ src/NHMCore/Nhmws/Models/LogsContentMessage.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHMCore.Nhmws.Models
+{
+    class LogsContentMessage
+    {
+        public string method => "executed";
+        [JsonProperty("params")]
+        public List<object> Params = new List<object>();
+        public string content = "";
+
+        public LogsContentMessage(int message_id, int error_code, string error_message, string logContent = "")
+        {
+            Params.Add(message_id);
+            Params.Add(error_code);
+            if (error_message != null)
+            {
+                Params.Add(error_message);
+            }
+            if(!string.IsNullOrEmpty(logContent))
+            {
+                content = logContent;
+            }
+        }
+
+        public string Serialize()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    } 
+}
---
+++ src/NHMCore/Nhmws/Models/LogsListMessage.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHMCore.Nhmws.Models
+{
+    class LogsListMessage
+    {
+        public string method => "executed";
+        [JsonProperty("params")]
+        public List<object> Params = new List<object>();
+        public List<string> data = new List<string>();
+
+        public LogsListMessage(int message_id, int error_code, string error_message, List<string> logsData = null)
+        {
+            Params.Add(message_id);
+            Params.Add(error_code);
+            if (error_message != null)
+            {
+                Params.Add(error_message);
+            }
+            if(logsData != null)
+            {
+                data = logsData;
+            }
+        }
+
+        public string Serialize()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    } 
+}
