dd8e24bc4bd90a0ad3ccbd794b67287d382f0663
luc1an24 <domen.kirnkrefl@nicehash.com>
2021-01-05 11:45:02 +0100

    update nbminer to 36.0

--- src/Miners/NBMiner/NBMinerPlugin.cs
+++ src/Miners/NBMiner/NBMinerPlugin.cs
@@ -26,11 +26,11 @@ namespace NBMiner
             // https://github.com/NebuTech/NBMiner/releases/ 
             MinersBinsUrlsSettings = new MinersBinsUrlsSettings
             {
-                BinVersion = "v35.2",
+                BinVersion = "v36.0",
                 ExePath = new List<string> { "NBMiner_Win", "nbminer.exe" },
                 Urls = new List<string>
                 {
-                    "https://github.com/NebuTech/NBMiner/releases/download/v35.2/NBMiner_35.2_Win.zip", // original
+                    "https://github.com/NebuTech/NBMiner/releases/download/v36.0/NBMiner_36.0_Win.zip", // original
                 }
             };
             PluginMetaInfo = new PluginMetaInfo
@@ -42,7 +42,7 @@ namespace NBMiner
 
         public override string PluginUUID => "f683f550-94eb-11ea-a64d-17be303ea466";
 
-        public override Version Version => new Version(15, 6);
+        public override Version Version => new Version(15, 7);
         public override string Name => "NBMiner";
 
         public override string Author => "info@nicehash.com";
