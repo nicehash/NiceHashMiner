37d7243848c18ab5b3a51f562f046cdb05e38315
object05 <alan.hablak@gmail.com>
2021-09-30 07:15:30 +0200

    Changed notification text to less aggressive

--- src/NHMCore/Notifications/AvailableNotifications.cs
+++ src/NHMCore/Notifications/AvailableNotifications.cs
@@ -493,13 +493,13 @@ namespace NHMCore.Notifications
 
         public static void CreateOutdatedNVIDIADriverWarning(Version minimum)
         {
-            var notification = new Notification(NotificationsType.Warning, NotificationsGroup.DriverNVIDIAObsolete, Tr("Older NVIDIA driver version detected"), Tr("Older NVIDIA driver version. Minimum is {0}. Please install latest NVIDIA drivers.", minimum.ToString()));
+            var notification = new Notification(NotificationsType.Warning, NotificationsGroup.DriverNVIDIAObsolete, Tr("Older NVIDIA driver version detected"), Tr("Older NVIDIA driver version was detected. Minimum is {0}. Please install latest NVIDIA drivers.", minimum.ToString()));
             NotificationsManager.Instance.AddNotificationToList(notification);
         }
 
         public static void CreateOutdatedAMDDriverWarning(Version minimum)
         {
-            var notification = new Notification(NotificationsType.Warning, NotificationsGroup.DriverAMDObsolete, Tr("Older AMD driver version detected"), Tr("Older AMD driver version. Minimum is Adrenalin {0}. Please install latest AMD drivers.", minimum.ToString()));
+            var notification = new Notification(NotificationsType.Warning, NotificationsGroup.DriverAMDObsolete, Tr("Older AMD driver version detected"), Tr("Older AMD driver version was detected. Minimum is Adrenalin {0}. Please install latest AMD drivers.", minimum.ToString()));
             NotificationsManager.Instance.AddNotificationToList(notification);
         }
     }
