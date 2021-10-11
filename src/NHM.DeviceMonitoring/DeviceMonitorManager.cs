24d64919ffa44713a77c9cc82a01c3bdff771dde
LAPTOP-DM3TLV0D\Stanko <krsticch@gmail.com>
2021-10-11 16:49:51 +0200

    device monitoring put UpdateVisitor in separate file

--- src/NHM.DeviceMonitoring/DeviceMonitorManager.cs
+++ src/NHM.DeviceMonitoring/DeviceMonitorManager.cs
@@ -98,21 +98,6 @@ namespace NHM.DeviceMonitoring
             });
         }
 
-        public class UpdateVisitor : IVisitor
-        {
-            public void VisitComputer(IComputer computer)
-            {
-                computer.Traverse(this);
-            }
-            public void VisitHardware(IHardware hardware)
-            {
-                hardware.Update();
-                foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
-            }
-            public void VisitSensor(ISensor sensor) { }
-            public void VisitParameter(IParameter parameter) { }
-        }
-
         public static bool IsMotherboardCompatible()
         {
             var isCompatible = true;
