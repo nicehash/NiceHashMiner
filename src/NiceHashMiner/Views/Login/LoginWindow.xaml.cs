7cd79540624a4580ed8109cfdace48ee3bd693aa
LAPTOP-DM3TLV0D\Stanko <krsticch@gmail.com>
2021-10-11 10:41:14 +0200

    fix login if no QR code

--- src/NiceHashMiner/Views/Login/LoginWindow.xaml.cs
+++ src/NiceHashMiner/Views/Login/LoginWindow.xaml.cs
@@ -88,71 +88,89 @@ namespace NiceHashMiner.Views.Login
             }
         }
 
-        private async Task ProcessQRCode()
+        [Serializable]
+        internal class BtcResponse
         {
-            var rigID = ApplicationStateManager.RigID();
-
-            var requestBody = "{\"qrId\":\"" + _uuid + "\", \"rigId\":\"" + rigID + "\"}";
-            var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
-
-            using (var client = new HttpClient())
-            {
-                var response = await client.PostAsync("https://api2.nicehash.com/api/v2/organization/nhmqr", content);
-            }
-            // create qr code
-            rect_qrCode.Fill = QrCodeHelpers.GetQRCode(_uuid);
-            Logger.Info("Login", $"QR code set with uuid: {_uuid}");
+            internal string btc { get; set; }
         }
 
         [Serializable]
-        private class BtcResponse
+        internal class RigUUIDRequest
         {
-            public string btc { get; set; }
+            internal string qrId { get; set; } = "";
+            internal string rigId { get; set; } = "";
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private static async Task<bool> RequestNew_QR_Code(string uuid, string rigId)
         {
-            await BtnClickTask();
+            try
+            {
+                var requestBody = JsonConvert.SerializeObject(new RigUUIDRequest { qrId = uuid, rigId = rigId });
+                using (var content = new StringContent(requestBody, Encoding.UTF8, "application/json"))
+                using (var client = new HttpClient())
+                {
+                    var response = await client.PostAsync("https://api2.nicehash.com/api/v2/organization/nhmqr", content);
+                    return response.IsSuccessStatusCode;
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Login.RequestNew_QR_Code", $"Got Exception: {e.Message}");
+                return false;
+            }
         }
 
-        private async Task BtnClickTask()
+        private async Task ProcessQRCode()
+        {
+            var requestSuccess = await RequestNew_QR_Code(_uuid, ApplicationStateManager.RigID());
+            if (requestSuccess)
+            {
+                // create qr code
+                rect_qrCode.Fill = QrCodeHelpers.GetQRCode(_uuid);
+            }
+            else
+            {
+                ScanLabel.Visibility = Visibility.Collapsed;
+                ScanConfirmButton.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private async void Confirm_Scan_Click(object sender, RoutedEventArgs e)
+        {
+            await Confirm_Scan_ClickTask();
+        }
+
+        private static async Task<string> GetBTCForUUID(string uuid)
         {
             try
             {
                 using (var client = new HttpClient())
                 {
-                    Logger.Info("Login", "Waiting for btc address");
-                    var resp = await client.GetAsync($"https://api2.nicehash.com/api/v2/organization/nhmqr/{_uuid}");
-                    if (resp.IsSuccessStatusCode)
-                    {
-                        var contentString = await resp.Content.ReadAsStringAsync();
-                        if (!string.IsNullOrEmpty(contentString))
-                        {
-                            var btcResp = JsonConvert.DeserializeObject<BtcResponse>(contentString);
-                            if (btcResp.btc != null)
-                            {
-                                if (CredentialValidators.ValidateBitcoinAddress(btcResp.btc))
-                                {
-                                    Logger.Info("Login", $"Got correct btc address: {btcResp.btc}");
-                                    CredentialsSettings.Instance.SetBitcoinAddress(btcResp.btc);
-                                    Close();
-                                }
-                                else
-                                {
-                                    Logger.Info("Login", $"Got incorrect btc address: {btcResp.btc}");
-                                }
-                            }
-                            else
-                            {
-                                Logger.Warn("Login", $"Didn't get correct btc response: {contentString}");
-                            }
-                        }
-                    }
+                    Logger.Info("LoginWindow.GetBTCForUUID", "Waiting for btc address");
+                    var resp = await client.GetAsync($"https://api2.nicehash.com/api/v2/organization/nhmqr/{uuid}");
+                    if (!resp.IsSuccessStatusCode) return null;
+                    var contentString = await resp.Content.ReadAsStringAsync();
+                    var btcResp = JsonConvert.DeserializeObject<BtcResponse>(contentString);
+                    var setBtc = btcResp?.btc;
+                    Logger.Info("LoginWindow.GetBTCForUUID", $"GetBTCForUUID Got btc address: {setBtc} for response: '{contentString}'");
+                    return setBtc;
                 }
             }
             catch (Exception ex)
             {
-                Logger.Error("Login", $"BtnClickTask failed with {ex.Message}");
+                Logger.Error("LoginWindow.GetBTCForUUID", $"GetBTCForUUID failed with {ex.Message}");
+                return null;
+            }
+        }
+
+        private async Task Confirm_Scan_ClickTask()
+        {
+            var btc = await GetBTCForUUID(_uuid);
+            if (CredentialValidators.ValidateBitcoinAddress(btc))
+            {
+                Logger.Info("LoginWindow.Confirm_Scan_ClickTask", $"Got valid btc address: {btc}");
+                CredentialsSettings.Instance.SetBitcoinAddress(btc);
+                Close();
             }
         }
     }
