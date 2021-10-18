ef32fe57817e88f0502108a44b51304299fdd3dc
Z1gismundus <ziga.tanacek@nicehash.com>
2021-10-18 12:14:55 +0200

    LibreHadware Installations fix

--- src/NHM.DeviceMonitoring/DeviceMonitorCPU.cs
+++ src/NHM.DeviceMonitoring/DeviceMonitorCPU.cs
@@ -45,12 +45,10 @@ namespace NHM.DeviceMonitoring
             {
                 if (!DeviceMonitorManager.IsElevated) return -1;
                 var temperature = -1;
-                var computer = new Computer();
+                var computer = DeviceSensorsMonitor.Instance.computer;
                 try
                 {
                     var updateVisitor = new UpdateVisitor();
-                    computer.Open();
-                    computer.IsCpuEnabled = true;
                     computer.Accept(updateVisitor);
                     var cpu = computer.Hardware.FirstOrDefault(hw => hw.HardwareType == HardwareType.Cpu);
                     var cpuSensors = cpu.Sensors.Where(s => s.SensorType == SensorType.Temperature);
@@ -63,10 +61,6 @@ namespace NHM.DeviceMonitoring
                 {
                     Logger.ErrorDelayed("DeviceMonitorCPU", "Error when getting CPU temperature: " + e.Message, _delayedLogging);
                 }
-                finally
-                {
-                    computer.Close();
-                }
 
                 return temperature;
             }
@@ -77,13 +71,10 @@ namespace NHM.DeviceMonitoring
             if (!DeviceMonitorManager.IsElevated) return (-1, -1);
             var percentage = -1;
             var ok = 0;
-            var computer = new Computer();
+            var computer = DeviceSensorsMonitor.Instance.computer;
             try
             {
                 var updateVisitor = new UpdateVisitor();
-                
-                computer.Open();
-                computer.IsMotherboardEnabled = true;
                 computer.Accept(updateVisitor);
                 var mainboard = computer.Hardware.FirstOrDefault(hw => hw.HardwareType == HardwareType.Motherboard);
                 foreach (var subHW in mainboard.SubHardware)
@@ -105,10 +96,6 @@ namespace NHM.DeviceMonitoring
             {
                 Logger.ErrorDelayed("DeviceMonitorCPU", "Error when getting CPU fan speed: " + e.Message, _delayedLogging);
                 ok = -1;
-            }
-            finally
-            {
-                computer.Close();
             }
 
             return (ok, percentage);
