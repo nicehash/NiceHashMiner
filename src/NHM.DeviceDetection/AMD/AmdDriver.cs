75be5f09361891d474e18a0157d6894a93bbb8be
LAPTOP-DM3TLV0D\Stanko <krsticch@gmail.com>
2021-10-19 10:41:02 +0200

    mini refactor

--- src/NHM.DeviceDetection/AMD/AmdDriver.cs
+++ src/NHM.DeviceDetection/AMD/AmdDriver.cs
@@ -10,81 +10,36 @@ namespace NHM.DeviceDetection.AMD
     {
         //Table of versions: https://gpuopen.com/version-table/
         public static readonly (Version DriveStoreFormat, Version AdrenalinFormat) MinimumVersion = (new Version(27,20,21003,8013), new Version(21,5,2));
-        private bool CorrectFormat;
-        public bool IsValid;
-        public bool IsCorrectVersion;
-        public AmdDriver(string DriveStoreVersion)
+        
+        public bool IsValid { get; private set; }
+        public bool IsCorrectVersion { get; private set; } // TODO rename
+
+        public Version VerDriverVersion { get; private set; }
+
+        public AmdDriver(string windowsDriverStoreVersion)
         {
-            _driverVersion = DriveStoreVersion;
-            IsCorrectVersion = IsValidVersion();
-            IsValid = CorrectFormat;
+            (IsValid, VerDriverVersion) = ToVersion(windowsDriverStoreVersion);
+            IsCorrectVersion = IsValid && VerDriverVersion >= MinimumVersion.DriveStoreFormat;
         }
 
-        private string _driverVersion { get; set; }
-        public string DriverVersion
+        private static bool IsValidWindowsVersion(string windowsDriverStoreVersion)
         {
-            get
-            {
-                return _driverVersion;
-            }
-        }
-        public Version VerDriverVersion;
-        private bool IsValidVersion()
-        {
-            string[] splitVerCurrent = DriverVersion.Split('.');
-            if (splitVerCurrent.Length != 4) return false;
-            VerDriverVersion = ToVersion();
-            bool res = IsMoreThanMinimumVersion(VerDriverVersion);
-            return res;
+            string[] splitVerCurrent = windowsDriverStoreVersion.Split('.');
+            return splitVerCurrent.Length == 4 && splitVerCurrent.All(s => int.TryParse(s, out var _));
         }
 
-        private bool IsMoreThanMinimumVersion(Version thisV)
+        private static (bool isValid, Version version) ToVersion(string windowsDriverStoreVersion)
         {
-            (int current, int min) delta = (-1,-1);
-            if (thisV.Major != MinimumVersion.DriveStoreFormat.Major) delta = (thisV.Major, MinimumVersion.DriveStoreFormat.Major);
-            else if (thisV.Minor != MinimumVersion.DriveStoreFormat.Minor) delta = (thisV.Minor, MinimumVersion.DriveStoreFormat.Minor);
-            else if (thisV.Build != MinimumVersion.DriveStoreFormat.Build) delta = (thisV.Build, MinimumVersion.DriveStoreFormat.Build);
-            else if (thisV.Revision != MinimumVersion.DriveStoreFormat.Revision) delta = (thisV.Revision, MinimumVersion.DriveStoreFormat.Revision);
-
-            if(delta != (-1, -1))
+            if (!IsValidWindowsVersion(windowsDriverStoreVersion)) return (false, null);
+            try
             {
-                if (delta.current >= delta.min) return true;
-                else return false;
+                return (true, new Version(windowsDriverStoreVersion));
             }
-
-            return true;
-        }
-
-
-        public Version ToVersion()
-        {
-            int major = -1;
-            int minor = -1;
-            int build = -1;
-            int rev = -1;
-            List<int> ver = new List<int>();
-            var splitVerCurrent = DriverVersion.Split('.');
-            if (splitVerCurrent.Length != 4)
+            catch (Exception)
             {
-                return new Version(major, minor, build, rev);
+                return (false, null);
             }
-
-            Int32.TryParse(splitVerCurrent[0], out major);
-            Int32.TryParse(splitVerCurrent[1], out minor);
-            Int32.TryParse(splitVerCurrent[2], out build);
-            Int32.TryParse(splitVerCurrent[3], out rev);
-
-
-            if (major == -1 || minor == -1 || build == -1 || rev == -1)
-            {
-                CorrectFormat = false;
-                return new Version(-1,-1,-1,-1);
-            }
-            else
-            {
-                CorrectFormat = true;
-                return new Version(major, minor, build, rev);
-            }
+            
         }
 
     }
