0136adf903e9e14297a62dea1474cee4c979950b
Z1gismundus <ziga.tanacek@nicehash.com>
2021-11-08 10:14:30 +0100

    lolminer update version

--- src/Miners/LolMiner/LolMinerPlugin.cs
+++ src/Miners/LolMiner/LolMinerPlugin.cs
@@ -25,11 +25,11 @@ namespace LolMiner
             // https://github.com/Lolliedieb/lolMiner-releases/releases | https://bitcointalk.org/index.php?topic=4724735.0 
             MinersBinsUrlsSettings = new MinersBinsUrlsSettings
             {
-                BinVersion = "1.33",
-                ExePath = new List<string> { "1.33", "lolMiner.exe" },
+                BinVersion = "1.35",
+                ExePath = new List<string> { "1.35", "lolMiner.exe" },
                 Urls = new List<string>
                 {
-                    "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.33/lolMiner_v1.33_Win64.zip" // original
+                    "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.35/lolMiner_v1.35_Win64.zip" // original
                 }
             };
             PluginMetaInfo = new PluginMetaInfo
