1a1a84c7eccba14b7b3e76f99fe67a9e2d369e59
object05 <alan.hablak@gmail.com>
2022-01-19 14:37:44 +0100

    fixed major versions instead of minor

--- src/Miners/NanoMiner/NanoMinerPlugin.cs
+++ src/Miners/NanoMiner/NanoMinerPlugin.cs
@@ -40,7 +40,7 @@ namespace NanoMiner
 
         public override string PluginUUID => "f25fee20-94eb-11ea-a64d-17be303ea466";
 
-        public override Version Version => new Version(16, 3);
+        public override Version Version => new Version(17, 0);
 
         public override string Name => "NanoMiner";
 
