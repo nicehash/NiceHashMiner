10570295def0a7e7677321a2cfb9ad46d4e4fe48
Z1gismundus <ziga.tanacek@nicehash.com>
2022-02-23 10:31:37 +0100

    nhm.commonwin32 project added

---
+++ src/NHM.CommonWin32/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHM.CommonWin32
+{
+    public class Class1
+    {
+    }
+}
