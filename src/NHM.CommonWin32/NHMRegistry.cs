bf69cbadf54e9bd7f7ddce1abae6da9425b17247
Z1gismundus <ziga.tanacek@nicehash.com>
2022-02-23 12:41:43 +0100

    functions moved to nhmregistry class

---
+++ src/NHM.CommonWin32/NHMRegistry.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using NHM.Common;
+using System;
+
+namespace NHM.CommonWin32
+{
+    public static class NHMRegistry
+    {
+        private static string NHM_SUBKEY => @"SOFTWARE\" + APP_GUID.GUID;
+        private static bool EnsureNHMSubKeyCalled = false;
+        private static void EnsureNHMSubKey()
+        {
+            if (EnsureNHMSubKeyCalled) return;
+            EnsureNHMSubKeyCalled = true;
+            try
+            {
+                using var key = Registry.CurrentUser.OpenSubKey(NHM_SUBKEY, false);
+                if (key == null) Registry.CurrentUser.CreateSubKey(NHM_SUBKEY);
+            }
+            catch (Exception e)
+            {
+                Logger.Error("NHMRegistry", $"EnsureNHMSubKey {e}");
+            }
+        }
+
+        public static int GetSubKey(string subKey)
+        {
+            EnsureNHMSubKey();
+            try
+            {
+                using var key = Registry.CurrentUser.OpenSubKey(NHM_SUBKEY, false);
+                var value = key?.GetValue(subKey) as string;
+                if (Int32.TryParse(value, out int TOSver)) return TOSver;
+                Logger.Warn("NHMRegistry", $"{subKey} was not read, defaulting to -1.");
+                return -1;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("NHMRegistry", $"GetSubKey {e}");
+                return -1;
+            }
+        }
+
+        public static void SetSubKey(string subKey, int value)
+        {
+            EnsureNHMSubKey();
+            try
+            {
+                using var key = Registry.CurrentUser.OpenSubKey(NHM_SUBKEY, true);
+                key.SetValue(subKey, value.ToString());
+            }
+            catch (Exception e)
+            {
+                Logger.Error("NHMRegistry", $"SetSubKey {subKey} {e}");
+            }
+        }
+
+    }
+}
--- src/NHMCore/Configs/ToSSetings.cs
+++ src/NHMCore/Configs/ToSSetings.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.Win32;
-using NHM.Common;
+﻿using NHM.Common;
+using NHM.CommonWin32;
 using System;
 
 namespace NHMCore.Configs
@@ -20,78 +20,22 @@ namespace NHMCore.Configs
                 OnPropertyChanged(nameof(Hwid));
             }
         }
-
-        private static string NHM_SUBKEY => @"SOFTWARE\" + APP_GUID.GUID;
-        private static bool EnsureNHMSubKeyCalled = false;
-        private static void EnsureNHMSubKey()
-        {
-            if (EnsureNHMSubKeyCalled) return;
-            EnsureNHMSubKeyCalled = true;
-            try
-            {
-                using (var key = Registry.CurrentUser.OpenSubKey(NHM_SUBKEY, false))
-                {
-                    if (key == null) Registry.CurrentUser.CreateSubKey(NHM_SUBKEY);
-                }
-            }
-            catch (Exception e)
-            {
-                Logger.Error("TOSSETTINGS", $"EnsureNHMSubKey {e}");
-            }        
-        }
-
-        private static int GetSubKey(string subKey)
-        {
-            EnsureNHMSubKey();
-            try
-            {
-                using (var key = Registry.CurrentUser.OpenSubKey(NHM_SUBKEY, false))
-                {
-                    var value = key?.GetValue(subKey) as string;
-                    if (Int32.TryParse(value, out int TOSver)) return TOSver;
-                    Logger.Warn("TOSSETTINGS", $"{subKey} was not read, defaulting to -1.");
-                    return -1;
-                }
-            }
-            catch (Exception e)
-            {
-                Logger.Error("TOSSETTINGS", $"GetSubKey {e}");
-                return -1;
-            }
-        }
-
-        private static void SetSubKey(string subKey, int value)
-        {
-            EnsureNHMSubKey();
-            try
-            {
-                using (var key = Registry.CurrentUser.OpenSubKey(NHM_SUBKEY, true))
-                {
-                    key.SetValue(subKey, value.ToString());
-                }
-            }
-            catch (Exception e)
-            {
-                Logger.Error("TOSSETTINGS", $"SetSubKey {subKey} {e}");
-            }
-        }
-
         public int AgreedWithTOS
         {
-            get { return GetSubKey(nameof(AgreedWithTOS)); }
+            get { return NHMRegistry.GetSubKey(nameof(AgreedWithTOS)); }
             set
             {
-                SetSubKey(nameof(AgreedWithTOS), value);
+                NHMRegistry.SetSubKey(nameof(AgreedWithTOS), value);
                 OnPropertyChanged(nameof(AgreedWithTOS));
             }
         }
 
         public int Use3rdPartyMinersTOS
         {
-            get { return GetSubKey(nameof(Use3rdPartyMinersTOS)); }
+            get { return NHMRegistry.GetSubKey(nameof(Use3rdPartyMinersTOS)); }
             set
             {
-                SetSubKey(nameof(Use3rdPartyMinersTOS), value);
+                NHMRegistry.SetSubKey(nameof(Use3rdPartyMinersTOS), value);
                 OnPropertyChanged(nameof(Use3rdPartyMinersTOS));
             }
         }
