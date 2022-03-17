34ae113e796bff27b2049db4ea6ba86c51afa45b
LAPTOP-DM3TLV0D\Stanko <krsticch@gmail.com>
2022-03-17 11:13:16 +0100

    update version

--- src/NiceHashMiner/Properties/AssemblyInfo.cs
+++ src/NiceHashMiner/Properties/AssemblyInfo.cs
@@ -64,5 +64,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.0.7.1")]
-[assembly: AssemblyFileVersion("3.0.7.1")]
+[assembly: AssemblyVersion("3.0.7.2")]
+[assembly: AssemblyFileVersion("3.0.7.2")]
