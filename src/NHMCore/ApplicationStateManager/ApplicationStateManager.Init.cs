a3d2b452ef1e6b61df1517d42f1a89b4c3f5e10e
Z1gismundus <ziga.tanacek@nicehash.com>
2022-04-12 12:14:13 +0200

    logic relocation

--- src/NHMCore/ApplicationStateManager/ApplicationStateManager.Init.cs
+++ src/NHMCore/ApplicationStateManager/ApplicationStateManager.Init.cs
@@ -180,22 +180,6 @@ namespace NHMCore
                 //        AvailableNotifications.CreateEnableComputeModeAMDInfo();
                 //    }
                 //}
-
-                if (AvailableDevices.HasGpuToPause)
-                {
-                    var deviceToPauseUuid = AvailableDevices.Devices.FirstOrDefault(dev => dev.PauseMiningWhenGamingMode && dev.DeviceType != DeviceType.CPU).Uuid;
-                    MiningSettings.Instance.DeviceIndex = AvailableDevices.GetDeviceIndexFromUuid(deviceToPauseUuid);
-                }
-                else if (MiningSettings.Instance.DeviceToPauseUuid != "")
-                {
-                    MiningSettings.Instance.DeviceIndex = AvailableDevices.GetDeviceIndexFromUuid(MiningSettings.Instance.DeviceToPauseUuid);
-                    AvailableDevices.GPUs.FirstOrDefault(dev => dev.Uuid == MiningSettings.Instance.DeviceToPauseUuid).PauseMiningWhenGamingMode = true;
-                }
-                else if (AvailableDevices.HasGpu)
-                {
-                    MiningSettings.Instance.DeviceIndex = 0;
-                    AvailableDevices.GPUs.FirstOrDefault().PauseMiningWhenGamingMode = true;
-                }
                 #endregion Device Detection
                 // STEP
                 // load plugins
@@ -294,8 +278,21 @@ namespace NHMCore
                 // Detected devices cross reference with miner indexes
                 await MinerPluginsManager.DevicesCrossReferenceIDsWithMinerIndexes(loader);
 
-                // This here is problematic because the GUI fires up file saving
-                MiningSettings.Instance.DeviceIndex = AvailableDevices.GetDeviceIndexFromUuid(MiningSettings.Instance.DeviceToPauseUuid);
+                if (AvailableDevices.HasGpuToPause)
+                {
+                    var deviceToPauseUuid = AvailableDevices.Devices.FirstOrDefault(dev => dev.PauseMiningWhenGamingMode && dev.DeviceType != DeviceType.CPU).Uuid;
+                    MiningSettings.Instance.DeviceIndex = AvailableDevices.GetDeviceIndexFromUuid(deviceToPauseUuid);
+                }
+                else if (MiningSettings.Instance.DeviceToPauseUuid != "")
+                {
+                    MiningSettings.Instance.DeviceIndex = AvailableDevices.GetDeviceIndexFromUuid(MiningSettings.Instance.DeviceToPauseUuid);
+                    AvailableDevices.GPUs.FirstOrDefault(dev => dev.Uuid == MiningSettings.Instance.DeviceToPauseUuid).PauseMiningWhenGamingMode = true;
+                }
+                else if (AvailableDevices.HasGpu)
+                {
+                    MiningSettings.Instance.DeviceIndex = 0;
+                    AvailableDevices.GPUs.FirstOrDefault().PauseMiningWhenGamingMode = true;
+                }
             }
             catch (Exception e)
             {
