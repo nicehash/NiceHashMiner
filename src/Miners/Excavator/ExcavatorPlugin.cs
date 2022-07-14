b4d6c1209b427150b921b484c77c564d5ef79b7c
S74nk0 <krsticch@gmail.com>
2022-07-14 13:23:46 +0200

    excavator testnet and testdev nhmp URL fix

--- src/Miners/Excavator/ExcavatorPlugin.cs
+++ src/Miners/Excavator/ExcavatorPlugin.cs
@@ -61,7 +61,7 @@ namespace Excavator
         }
 
 #if LHR_BUILD_ON
-        public override Version Version => new Version(17, 1);
+        public override Version Version => new Version(17, 2);
 #else
         public override Version Version => new Version(16, 3);
 #endif
