fecade8f29dd38f85516e4190809b923cb926003
S74nk0 <krsticch@gmail.com>
2022-07-21 08:57:22 +0200

    fix demo mining notice on startup

--- src/NiceHashMiner/Views/MainWindow.xaml.cs
+++ src/NiceHashMiner/Views/MainWindow.xaml.cs
@@ -92,10 +92,10 @@ namespace NiceHashMiner.Views
                     ShowContentAsModalDialog(nhmUpdaterDialog);
                 });
             };
+            NotificationsManager.Instance.PropertyChanged += NotificationsManagerInstance_PropertyChanged;
+            MiningState.Instance.PropertyChanged += MiningStateInstance_PropertyChanged;
             await MainWindow_OnLoadedTask();
             _vm.GUISettings.PropertyChanged += GUISettings_PropertyChanged;
-            NotificationsManager.Instance.PropertyChanged += Instance_PropertyChanged;
-            MiningState.Instance.PropertyChanged += MiningStateInstance_PropertyChanged;
             SetNotificationCount(NotificationsManager.Instance.NotificationNewCount);
 
             if (!HasWriteAccessToFolder(Paths.Root))
@@ -128,7 +128,7 @@ namespace NiceHashMiner.Views
             }
         }
 
-        private void Instance_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        private void NotificationsManagerInstance_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             if (nameof(NotificationsManager.NotificationNewCount) == e.PropertyName)
             {
