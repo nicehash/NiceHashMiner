4c6b1a90e7ef181bb32f99b47c93d6f6cd13dbcc
alanH1998 <alan.hablak@nicehash.com>
2022-09-28 12:56:24 +0200

    fixed instance splitting

--- src/NHM.MinerPluginToolkitV1/CommandLine/MinerExtraParameters.cs
+++ src/NHM.MinerPluginToolkitV1/CommandLine/MinerExtraParameters.cs
@@ -114,7 +114,7 @@ namespace NHM.MinerPluginToolkitV1.CommandLine
                 foreach (var b in devicesParameters)
                 {
                     if (a == b) continue;
-                    if(a.Count == 0 || b.Count == 0) return false;
+                    if(a.Count == 0 ^ b.Count == 0) return false;
                     if (!CheckIfCanGroup(a, b)) return false;
                 }
             }
