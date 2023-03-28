2210d0afb5bc4b22617a399273720784a1b1ccad
object99 <alan.hablak@nicehash.com>
2023-03-28 08:04:10 +0200

    Version update

--- src/NiceHashMinerLauncher/Properties/AssemblyInfo.cs
+++ src/NiceHashMinerLauncher/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.1.0.5")]
-[assembly: AssemblyFileVersion("3.1.0.5")]
+[assembly: AssemblyVersion("3.1.0.6")]
+[assembly: AssemblyFileVersion("3.1.0.6")]
