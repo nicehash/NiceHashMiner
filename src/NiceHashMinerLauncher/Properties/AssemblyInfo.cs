e7a6ca2e24f17f5999d887234a0ad2f3cf750564
object05 <alan.hablak@gmail.com>
2023-07-20 14:02:10 +0200

    version increase

--- src/NiceHashMinerLauncher/Properties/AssemblyInfo.cs
+++ src/NiceHashMinerLauncher/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.1.0.8")]
-[assembly: AssemblyFileVersion("3.1.0.8")]
+[assembly: AssemblyVersion("3.1.0.9")]
+[assembly: AssemblyFileVersion("3.1.0.9")]
