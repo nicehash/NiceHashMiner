d9520a33f3beb2df253f63d4f9b28f6d21f22118
object05 <alan.hablak@gmail.com>
2023-08-31 09:55:10 +0200

    removed nbminer from preinstalled, added gminer as preinstalled

--- src/Tools/NhmPackager/MinerPluginsPacker.cs
+++ src/Tools/NhmPackager/MinerPluginsPacker.cs
@@ -142,11 +142,11 @@ namespace NhmPackager
         public static readonly IReadOnlyList<string> PreInstalledPlugins = new string[]
             {
                 //"e294f620-94eb-11ea-a64d-17be303ea466", // CryptoDredge
-                //"d8ddcaf2-95c5-4f9a-b65f-c123a0d4fbc2", // GMiner
+                "d8ddcaf2-95c5-4f9a-b65f-c123a0d4fbc2", // GMiner
                 "eb75e920-94eb-11ea-a64d-17be303ea466", // LolMiner
                 //"eda6abd0-94eb-11ea-a64d-17be303ea466", // MiniZ - BROKEN
                 //"f25fee20-94eb-11ea-a64d-17be303ea466", // NanoMiner
-                "f683f550-94eb-11ea-a64d-17be303ea466", // NBMiner
+                //"f683f550-94eb-11ea-a64d-17be303ea466", // NBMiner
                 //"fa369d10-94eb-11ea-a64d-17be303ea466", // Phoenix
                 //"fd45fff0-94eb-11ea-a64d-17be303ea466", // SRBMiner
                 //"01177a50-94ec-11ea-a64d-17be303ea466", // TeamRedMiner
