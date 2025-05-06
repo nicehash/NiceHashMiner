70dfc956c7a454f9adc1596d9b84db5a1852968a
alanH1998 <alan.hablak@nicehash.com>
2025-05-06 13:01:18 +0200

    updated lolminer

--- src/Miners/LolMiner/LolMinerPlugin.cs
+++ src/Miners/LolMiner/LolMinerPlugin.cs
@@ -25,11 +25,11 @@ namespace LolMiner
             // https://github.com/Lolliedieb/lolMiner-releases/releases | https://bitcointalk.org/index.php?topic=4724735.0 
             MinersBinsUrlsSettings = new MinersBinsUrlsSettings
             {
-                BinVersion = "1.92",
-                ExePath = new List<string> { "1.92", "lolMiner.exe" },
+                BinVersion = "1.95a",
+                ExePath = new List<string> { "1.95a", "lolMiner.exe" },
                 Urls = new List<string>
                 {
-                    "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.92/lolMiner_v1.92_Win64.zip" // original
+                    "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.95a/lolMiner_v1.95a_Win64.zip" // original
                 }
             };
             PluginMetaInfo = new PluginMetaInfo
@@ -39,7 +39,7 @@ namespace LolMiner
             };
         }
 
-        public override Version Version => new Version(24, 3);
+        public override Version Version => new Version(24, 4);
 
         public override string Author => "info@nicehash.com";
 
