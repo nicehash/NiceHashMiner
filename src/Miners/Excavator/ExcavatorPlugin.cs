0591d3cf327fc608da35bbaa2ad1553fd5ec2a10
object99 <alan.hablak@nicehash.com>
2025-10-01 09:55:45 +0200

    updated excavator and lolminer versions

--- src/Miners/Excavator/ExcavatorPlugin.cs
+++ src/Miners/Excavator/ExcavatorPlugin.cs
@@ -27,11 +27,11 @@ namespace Excavator
 
             MinersBinsUrlsSettings = new MinersBinsUrlsSettings
             {
-                BinVersion = "v1.8.12.0",
-                ExePath = new List<string> { "NHQM_v0.6.12.0", "excavator.exe" },
+                BinVersion = "v1.9.1.0",
+                ExePath = new List<string> { "NHQM_v0.6.13.0", "excavator.exe" },
                 Urls = new List<string>
                 {
-                    "https://github.com/nicehash/NiceHashQuickMiner/releases/download/v0.6.12.0/NHQM_v0.6.12.0.zip"
+                    "https://github.com/nicehash/NiceHashQuickMiner/releases/download/v0.6.13.0/NHQM_v0.6.13.0.zip"
                 }
             };
             PluginMetaInfo = new PluginMetaInfo
@@ -41,7 +41,7 @@ namespace Excavator
             };
         }
 
-        public override Version Version => new Version(25, 0);
+        public override Version Version => new Version(25, 1);
 
         public override string PluginUUID => "27315fe0-3b03-11eb-b105-8d43d5bd63be";
         public override string Name => "Excavator";
